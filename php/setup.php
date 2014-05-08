51f5ee05de7ed06c50fb3028915c6afa8625658b
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-05-08 12:31:45 +0000

    Read database info from local/config.inc

---
+++ php/setup.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Derby Set-Up</title>
+</head>
+<body>
+<!-- TODO: Something more helpful. -->
+<h2>Derby Set-Up</h2>
+<h3>Please construct a local/config.inc file in your site directory.</h3>
+</body>
+</html>
