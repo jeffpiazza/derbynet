32db576ac38f7dc51c5ec2ee235099b4b2ef818a
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-05-09 12:58:57 +0000

    'schedule' action; read_single_value tests for null; action.php direct dispatch; remove jquery.min.map reference from jquery.js

--- php/action.php
+++ php/action.php
@@ -13,21 +13,8 @@ header('Content-Type: text/xml');
 
 require_once('action/helpers.inc');
 
-if ($_POST['action'] == 'pass') {
-	require('action/action.pass.inc');
-} else if ($_POST['action'] == 'xbs') {
-	require('action/action.xbs.inc');
-} else if ($_POST['action'] == 'renumber') {
-	require('action/action.renumber.inc');
-} else if ($_POST['action'] == 'classchange') {
-	require('action/action.classchange.inc');
-} else if ($_POST['action'] == 'photo') {
-	require('action/action.photo.inc');
-} else if ($_POST['action'] == 'initaudit') {
-	require('action/action.initaudit.inc');
-} else if ($_POST['action'] == 'initnumbers') {
-	require('action/action.initnumbers.inc');
-} else {
+if (!@include 'action/action.'.$_POST['action'].'.inc') {
     echo '<checkin><failure>Unrecognized post: '.$_POST['action'].'</failure></checkin>';
 }
+
 ?>
