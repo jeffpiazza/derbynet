a143c5cc880d64ef74ba0ad6473a0aa1c5a56e57
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-05-17 18:09:22 +0000

    Identifying kiosks

--- website/action.php
+++ website/action.php
@@ -1,20 +1,33 @@
 <?php @session_start(); ?>
 <?php
 
-// Receive POSTs from the checkin page to perform check-in actions,
-// return XML responses
-
+// Receive POSTs to perform actions, return XML responses
+//
+// <action-response> is document root of reply, constructed from $_POST arguments
+//
 require_once('inc/data.inc');
 require_once('inc/permissions.inc');
 require_once('inc/authorize.inc');
 
 
-header('Content-Type: text/xml');
-
 require_once('action/helpers.inc');
 
-if (!@include 'action/action.'.$_POST['action'].'.inc') {
-    echo '<checkin><failure>Unrecognized post: '.$_POST['action'].'</failure></checkin>';
+if (!empty($_POST)) {
+    header('Content-Type: text/xml');
+    if (!@include 'action/action.'.$_POST['action'].'.inc') {
+        start_response();
+        echo '<failure code="unrecognized">Unrecognized action: '.@$_POST['action'].'</failure>';
+        end_response();
+    }
+} else if (!empty($_GET)) {
+    header('Content-Type: text/xml');
+    if (!@include 'action/query.'.$_GET['query'].'.inc') {
+        start_response();
+        echo '<failure code="unrecognized">Unrecognized query: '.@$_GET['query'].'</failure>';
+        end_response();
+    }
+} else {
+    echo '<html><head><title>Not a Page</title></head><body><h1>This is not a page.</h1></body></html>';
 }
 
 ?>
