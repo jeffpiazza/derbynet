d908e2005ea68ef4e2d524cc222b96a032c3b353
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-05-26 16:54:10 +0000

    Debugging Champ timer

--- timer/src/org/jeffpiazza/derby/TimerTest.java
+++ timer/src/org/jeffpiazza/derby/TimerTest.java
@@ -9,7 +9,13 @@ public class TimerTest implements TimerDevice.RaceFinishedCallback, TimerDevice.
     public static TimerDevice identifyTimerDevice(SerialPort port) {
         System.out.println("Trying " + port.getPortName());
         try {
-            SerialPortWrapper wrapper = new SerialPortWrapper(port);
+	    File logfile = new File("timer.log");
+	    if (!logfile.exists()) {
+		logfile.createNewFile();
+	    }
+            SerialPortWrapper wrapper = new SerialPortWrapper(port, 
+							      new PrintWriter(new BufferedWriter(new FileWriter(logfile)),
+									      /*autoflush*/true));
 
             // TODO-----------
             TimerDevice[] devices = { new ChampDevice(wrapper),
@@ -32,15 +38,14 @@ public class TimerTest implements TimerDevice.RaceFinishedCallback, TimerDevice.
     }
 
     public static TimerDevice identifyTimerDevice() {
-		File[] files = new File("/dev").listFiles(new FilenameFilter() {
-				public boolean accept(File dir, String name) {
-					return name.startsWith("tty") && !name.equals("tty") && name.contains("usb");
-				}
-			});
+	File[] files = new File("/dev").listFiles(new FilenameFilter() {
+		public boolean accept(File dir, String name) {
+		    return name.startsWith("tty") && !name.equals("tty") && 
+			(name.contains("usb") || name.contains("USB"));
+		}
+	    });
 
-        System.out.println("identifyTimerDevice: " + files.length);  // TODO
-
-		for (File f : files) {
+	for (File f : files) {
             SerialPort port = new SerialPort(f.getPath());
             System.out.println("Opening " + f.getPath());
             try {
@@ -58,12 +63,12 @@ public class TimerTest implements TimerDevice.RaceFinishedCallback, TimerDevice.
                 return device;
             }
 
-			try {
-				port.closePort();
-			}
-			catch (SerialPortException exc) {
-				System.out.println("Caught exception " + exc + " while closing");
-			}
+	    try {
+		port.closePort();
+	    }
+	    catch (SerialPortException exc) {
+		System.out.println("Caught exception " + exc + " while closing");
+	    }
         }
 
         return null;
@@ -85,19 +90,26 @@ public class TimerTest implements TimerDevice.RaceFinishedCallback, TimerDevice.
     }
 
     public void runTestLoop(TimerDevice device) throws SerialPortException {
+	int mask = 0;
         while (true) {
-            device.prepareHeat(7);
+	    -- mask;
+	    if (mask <= 0) { mask = 7; } // TODO: Generalize
+
+	    System.out.println("prepareHeat(" + mask + ")");  // TODO
+            device.prepareHeat(mask);
             raceRunning = true;
             
-            long heatDeadline = System.currentTimeMillis() + 20000;
-            while (raceRunning && System.currentTimeMillis() < heatDeadline) {
+            long heatDeadline = System.currentTimeMillis() + 30000;
+            while (raceRunning) { // TODO && System.currentTimeMillis() < heatDeadline) {
                 device.poll();
                 try {
-                    Thread.sleep(250);  // Poll 4 times per second
+                    Thread.sleep(50);  // ms.
                 } catch (Exception exc) {}
             }
+	    if (raceRunning) System.out.println("Abandoning heat (wrong choice!)");
 
             try {
+		System.out.println("Pausing before starting a new heat.");
                 Thread.sleep(4000);  // 4-second pause between heats
             } catch (Exception exc) {}
         }
