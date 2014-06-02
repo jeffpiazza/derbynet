63cd94f0f38da74c9ac84cc3210a16e051a20d87
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-06-02 00:46:33 +0000

    Split out photo-thumbs javascript and css into separate files

---
+++ website/js/photo-thumbs.js
@@ -0,0 +1,129 @@
+// Approximate structure for the photo-thumbs page:
+//
+// <body>
+//   (refresh button)
+//   <div body-wrapper>
+//      <div thumblist>
+//        <ul>
+//           <li>...   -- has without-photo class if accepting new photos
+//        </ul>
+//      </div>
+//
+//      <div photothumbs>
+//         <div thumbnail>
+//            <a>
+//               <img unassigned-photo />
+//            </a>...
+//         </div>
+//      </div>
+//   </div>
+// </body>
+
+// draggable and droppable elements with scope: "assign" are for
+// dragging unassigned photos to racers who are still without a photo.
+//
+// scope: "undo" is for removing a photo assignment, by dragging from
+// the list element back to the photothumbs div.
+
+  // target is a jquery for list items that should be made assignable,
+  // i.e., can receive new photo assignments.
+  function make_assignable(target) {
+	  target.droppable({
+		scope: "assign",
+		hoverClass: "droppableHover",
+		drop: function(event, ui) {
+			var photo_base_name = ui.draggable.attr("data-image-filename");
+			var racer_id = $(this).attr("data-racer-id");
+
+			changeRacerPhotoAjax(0, racer_id, photo_base_name);
+
+			ui.draggable.closest(".thumbnail").addClass("hidden");
+			$(this).prepend('<img class="assigned"' +
+							' data-image-filename="' + photo_base_name + '"' +
+							' src="photo-fetch.php/tiny/' +
+							encodeURIComponent(photo_base_name) + '"/>');
+
+			make_discardable($(this).find(".assigned"));
+			// Once dropped, make no longer droppable!
+			$(this).droppable("destroy");
+		  }
+		});
+  }
+
+  function make_discardable(target) {
+	target.draggable({
+		    revert: 'invalid',
+			scope: 'undo',
+
+		    helper: 'clone',
+		    appendTo: 'body',
+		    opacity: 0.5,
+			});
+  }
+
+  $(function() {
+	  make_assignable($(".without-photo"));
+	  $(".unassigned-photo").draggable({
+		    helper: 'clone',
+			appendTo: 'body',
+			// When using helper: clone, the clone's top left corner
+			// appears to be what controls selection of the droppable
+			// target, and I don't see any way to change it to use the
+			// mouse cursor instead.  If you can't beat 'em, join 'em:
+			// use cursorAt to force the clone's top to match the
+			// cursor.  (For picking from the list of racers, we don't
+			// care so much about horizontal position.)
+
+			// TODO: Don't know why only top has effect here.
+			cursorAt: { top: 0, left: 20 },
+			opacity: 0.5,
+		    revert: 'invalid',
+			scope: "assign",
+			});
+
+	  $(".photothumbs").droppable({
+		    hoverClass: 'droppableHover',
+			scope: 'undo',
+			drop: function(event, ui) {
+			  // ui.draggable will be the assigned <img> itself
+			  var image_filename = ui.draggable.attr("data-image-filename");
+			  console.log("discarded image filename = " + image_filename);
+
+			  var list_item = ui.draggable.closest("li");
+			  list_item.addClass("without-photo");
+			  make_assignable(list_item);
+
+			  ui.draggable.remove();
+			  var hidden_thumbnail = $("img[data-image-filename='" + image_filename + "']").closest(".thumbnail");
+			  hidden_thumbnail.removeClass("hidden");
+			  // A previous drag may have added a style property with
+			  // top and left values that will leave the image out of
+			  // place; we need to clear them.
+			  hidden_thumbnail.find("img")
+				.css("top", "")
+				.css("left", "");
+			  removeRacerPhoto(list_item.attr("data-racer-id"));
+		  }
+		});
+
+	  make_discardable($(".assigned"));
+	});
+
+
+function changeRacerPhotoAjax(previous, racer, photo) {
+  console.log('changeRacerPhotoAjax: previous racer = ' + previous
+			  + ', new racer = ' + racer + ', photo = ' + photo);
+
+   var xmlhttp = new XMLHttpRequest();
+   xmlhttp.open("POST", g_checkin_action_url, /*async*/true);
+   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+   xmlhttp.onreadystatechange = readystate_handler;
+   xmlhttp.send("action=photo&racer=" + racer
+				+ "&previous=" + previous
+				+ "&photo=" + encodeURIComponent("<?php echo addcslashes($photoThumbsDirectory.'\\', '\\\"'); ?>" + photo));
+   ajax_add_request();
+}
+
+function removeRacerPhoto(previous) {
+  changeRacerPhotoAjax(previous, 0, '');
+}
--- website/photo-thumbs.php
+++ website/photo-thumbs.php
@@ -44,146 +44,12 @@ $allfiles = scan_directory($photoOriginalsDirectory,
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="css/jquery.mobile-1.4.2.css"/>
 <?php require('inc/stylesheet.inc'); ?>
+<link rel="stylesheet" type="text/css" href="css/photo-thumbs.css"/>
 <script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/jquery-ui-1.10.4.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.10.4.min.js"></script>
 <script type="text/javascript" src="js/jquery.ui.touch-punch.min.js"></script>
 <script type="text/javascript" src="js/checkin.js"></script>
-<script type="text/javascript">
-  // target is a jquery for list items that should be made assignable,
-  // i.e., can receive new photo assignments.
-  function make_assignable(target) {
-	  target.droppable({
-		scope: "assign",
-		hoverClass: "droppableHover",
-		drop: function(event, ui) {
-			var photo_base_name = ui.draggable.attr("data-image-filename");
-			var racer_id = $(this).attr("data-racer-id");
-
-			console.log('drop: photo_base_name = ' + photo_base_name);
-			console.log('drop: urlencode(photo_base_name) = ' +
-						encodeURIComponent(photo_base_name));
-			console.log('drop: racer_id = ' + racer_id);
-
-			changeRacerPhotoAjax(0, racer_id, photo_base_name);
-			ui.draggable.closest(".thumbnail").addClass("hidden");
-			$(this).prepend('<img class="assigned"' +
-							' data-image-filename="' + photo_base_name + '"' +
-							' src="photo-fetch.php/tiny/' +
-							encodeURIComponent(photo_base_name) + '"/>');
-			make_discardable($(this).find(".assigned"));
-			// Once dropped, make no longer droppable!
-			$(this).droppable("destroy");
-		  }
-		});
-  }
-
-  function make_discardable(target) {
-	target.draggable({
-		    revert: 'invalid',
-			scope: 'undo',
-
-		    helper: 'clone',
-		    appendTo: 'body',
-		    opacity: 0.5,
-			});
-  }
-
-  $(function() {
-	  make_assignable($(".without-photo"));
-	  $(".unassigned-photo").draggable({
-		    helper: 'clone',
-			appendTo: 'body',
-			// When using helper: clone, the clone's top left corner
-			// appears to be what controls selection of the droppable
-			// target, and I don't see any way to change it to use the
-			// mouse cursor instead.  If you can't beat 'em, join 'em:
-			// use cursorAt to force the clone's top to match the
-			// cursor.  (For picking from the list of racers, we don't
-			// care so much about horizontal position.)
-
-			// TODO: Don't know why only top has effect here.
-			cursorAt: { top: 0, left: 20 },
-			opacity: 0.5,
-		    revert: 'invalid',
-			scope: "assign",
-			});
-
-	  $(".photothumbs").droppable({
-		    hoverClass: 'droppableHover',
-			scope: 'undo',
-			drop: function(event, ui) {
-			  // ui.draggable will be the assigned <img> itself
-			  var image_filename = ui.draggable.attr("data-image-filename");
-			  console.log("discarded image filename = " + image_filename);
-
-			  var list_item = ui.draggable.closest("li");
-			  list_item.addClass("without-photo");
-			  make_assignable(list_item);
-
-			  ui.draggable.remove();
-			  var hidden_thumbnail = $("img[data-image-filename='" + image_filename + "']").closest(".thumbnail");
-			  hidden_thumbnail.removeClass("hidden");
-			  // A previous drag may have added a style property with
-			  // top and left values that will leave the image out of
-			  // place; we need to clear them.
-			  hidden_thumbnail.find("img")
-				.css("top", "")
-				.css("left", "");
-			  removeRacerPhoto(list_item.attr("data-racer-id"));
-		  }
-		});
-
-	  make_discardable($(".assigned"));
-	});
-
-</script>
-<style type="text/css">
-/* CSS adapted from
-http://blog.stevensanderson.com/2011/10/05/full-height-app-layouts-a-css-trick-to-make-it-easier/
-*/
-
-body { margin: 0; }
-
-.body-wrapper { /* body row */
- position: absolute;
- top: 128px;
- bottom: 0;
- left: 0;
- right: 0;
- overflow: hidden;
-}
-
-.thumblist {
- position: absolute;
- top: 0;
- bottom: 0;
- left: 0;
- width: 300px;
- overflow-x: hidden;
- overflow-y: auto;
- z-index: 1;
-}
-
-.thumblist li { z-index: 2; }
-
-.photothumbs {
- position: absolute;
- top: 0;
- bottom: 0;
- left: 300px;
- right: 0;
-   overflow-x: visible;
- overflow-y: auto;
- z-index: 1;
-}
-
-img { z-index: 30; }
-
-.droppableHover {
-  background-color: #ffffb4;
-}
-
-</style>
+<script type="text/javascript" src="js/photo-thumbs.js"></script>
 </head>
 <body>
 <?php $banner_title = 'Racer Photos'; require('inc/banner.inc'); ?>
@@ -271,26 +137,6 @@ if (empty($allfiles)) {
 </div>
 
 </div>
-<script type="text/javascript">
-
-function changeRacerPhotoAjax(previous, racer, photo) {
-  console.log('changeRacerPhotoAjax: previous racer = ' + previous
-			  + ', new racer = ' + racer + ', photo = ' + photo);
-
-   var xmlhttp = new XMLHttpRequest();
-   xmlhttp.open("POST", g_checkin_action_url, /*async*/true);
-   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-   xmlhttp.onreadystatechange = readystate_handler;
-   xmlhttp.send("action=photo&racer=" + racer
-				+ "&previous=" + previous
-				+ "&photo=" + encodeURIComponent("<?php echo addcslashes($photoThumbsDirectory.'\\', '\\\"'); ?>" + photo));
-   ajax_add_request();
-}
-
-function removeRacerPhoto(previous) {
-  changeRacerPhotoAjax(previous, 0, '');
-}
-</script>
 <div id="ajax_working" class="hidden">
   <span id="ajax_num_requests">0</span> request(s) pending.
 </div>
