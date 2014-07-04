53c39a283dd4a05a313dc484c4c3140a40b5281d
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-07-04 01:46:29 +0000

    Remove classchange and renumber actions in favor of unified edit-racer action; checkin page single change form.

--- website/js/checkin.js
+++ website/js/checkin.js
@@ -61,70 +61,121 @@ function handlechange_xbs(cb) {
            });
 }
 
-// This executes when a car's number cell is clicked; we present an
-// overlaid form to renumber the car.
-function show_renumber_form(racer_name, racerid, td) {
-  $("#renumber_racerprompt").html(racer_name);
-  $("#renumber_racer").val(racerid);
-  $("#renumber_carno").val(td.innerHTML);
-  $("#renumberform").removeClass("hidden");
-  $("#renumber_carno").focus();
+function show_edit_racer_form(racerid) {
+  var first_name = $('#firstname-' + racerid).text();
+  var last_name = $('#lastname-' + racerid).text();
+  var car_no = $('#car-number-' + racerid).text();
+
+  var rankid = $('#class-' + racerid).attr('data-rankid');
+
+  $("#editracerform").removeClass("hidden");
+
+  $("#edit_racer").val(racerid);
+
+  $("#edit_firstname").val(first_name);
+  $("#edit_lastname").val(last_name);
+
+  $("#edit_carno").val(car_no);
+  $("#edit_carno").focus();
+
+  var edit_rank = $("#edit_rank");
+  edit_rank.val(rankid);
+  // I think it's a bug in jquery-mobile that an explicit change
+  // event is required; setting the val above should be sufficient
+  // to cause an update.
+  edit_rank.change();
 }
 
-// This runs when the submit button for the car-renumbering form is
-// clicked.
-function handle_renumber() {
-    var racerid = $("#renumber_racer").val();
-    var new_carno = $("#renumber_carno").val();
-    $.ajax(g_checkin_action_url,
-           {type: 'POST',
-             data: {action: 'renumber',
-                   racer: racerid,
-                   value: new_carno},
+function handle_edit_racer() {
+  var racerid = $("#edit_racer").val();
+
+  var new_firstname = $("#edit_firstname").val();
+  var new_lastname = $("#edit_lastname").val();
+  var new_carno = $("#edit_carno").val();
+
+  var rank_picker = $("#edit_rank");
+  var new_rankid = rank_picker.val();
+
+  var rank_option = $('[value="' + new_rankid + '"]', rank_picker);
+  var new_classname = rank_option.attr('data-class');
+  var new_rankname = rank_option.attr('data-rank');
+
+  $.ajax(g_checkin_action_url,
+         {type: 'POST',
+          data: {action: 'edit-racer',
+                 racer: racerid,
+                 firstname: new_firstname,
+                 lastname: new_lastname,
+                 carno: new_carno,
+                 rankid: new_rankid},
             success: function(data) {
-                $("#renumber" + racerid).html(new_carno);
-            },
-           });
-   $("#renumberform").addClass("hidden");
-}
+                console.log("handle_edit_racer success()!");
+                console.log(new_firstname);
+                $("#firstname-" + racerid).text(new_firstname);
+                $("#lastname-" + racerid).text(new_lastname);
+                $("#car-number-" + racerid).text(new_carno);
 
+                $('#class-' + racerid).attr('data-rankid', new_rankid);
+                $('#class-' + racerid).text(new_classname);
+                $('#rank-' + racerid).text(new_rankname);
 
-function show_rank_change_form(racer_name, racerid, rankid, td) {
-    var rank_picker = $("#rank_picker");
-
-    $("#rank_racerprompt").html(racer_name);
-    $("#rank_racer").val(racerid);
-
-    // $("option", rank_picker).removeProp("selected");
-    // $("[value=" + rankid + "]", rank_picker).prop("selected", true);
-    rank_picker.val(rankid);
-    // I think it's a bug in jquery-mobile that an explicit change
-    // event is required; setting the val above should be sufficient
-    // to cause an update.
-    rank_picker.change();
-
-    $("#rankchangeform").removeClass("hidden");
-    rank_picker.focus();
-}
-
-function handle_rank_change() {
-    var racerid = $("#rank_racer").val();
-    var rank_picker = $("#rank_picker");
-    var value = rank_picker.val();
-    var option = $('[value="' + value + '"]', rank_picker);
-    var classname = option.attr('data-class');
-    var rankname = option.attr('rank-class');
-
-    $.ajax(g_checkin_action_url,
-           {type: 'POST',
-             data: {action: 'classchange',
-                   racer: racerid,
-                   value: value},
-            success: function(data) {
-                $('#class-' + racerid).text(classname);
-                $('#rank-' + racerid).text(rankname);
+                sort_checkin_table();
             },
            });
 
-   $("#rankchangeform").addClass("hidden");
+   $("#editracerform").addClass("hidden");
+}
+
+
+function compare_first(a, b) {
+  if (a[0] == b[0])
+      return 0;
+  if (a[0] < b[0])
+      return -1;
+   return 1;
+}
+
+
+function sorting_key(row) {
+  if (g_order == 'den') {
+      // class, lastname, firstname
+      return [row.getElementsByClassName('sort-class')[0].innerHTML,
+              row.getElementsByClassName('sort-lastname')[0].innerHTML,
+              row.getElementsByClassName('sort-firstname')[0].innerHTML]
+  } else if (g_order == 'car') {
+      // carnumber (numeric), lastname, firstname
+      return [parseInt(row.children('.sort-car-number')[0].innerHTML),
+              row.getElementsByClassName('sort-lastname')[0].innerHTML,
+              row.getElementsByClassName('sort-firstname')[0].innerHTML]
+  } else /* 'name' */ {
+      // lastname, firstname
+      return [row.getElementsByClassName('sort-lastname')[0].innerHTML,
+              row.getElementsByClassName('sort-firstname')[0].innerHTML]
+  }
+}
+
+function sort_checkin_table() {
+	row_array = [];
+    rows = $(".main_table tbody").get(0).getElementsByTagName('tr');
+
+    console.log("rows"); console.log(rows);
+
+	for (var j = 0; j < rows.length; ++j) {
+	    row_array[row_array.length] = [sorting_key(rows[j]), rows[j]];
+	}
+
+	row_array.sort(compare_first);
+
+	tb = $(".main_table tbody").get(0);
+
+    console.log("tb"); console.log(tb);
+
+	for (var j = 0; j < row_array.length; ++j) {
+        row_array[j][1].classList.remove('d' + (j & 1));
+        row_array[j][1].classList.add('d' + ((j + 1) & 1));
+        console.log(row_array[j][1].classList);
+	    tb.appendChild(row_array[j][1]);
+	}
+
+	delete row_array;
 }
