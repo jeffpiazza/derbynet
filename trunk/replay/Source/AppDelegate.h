17f8e448cf655f36ce549fd006128b71c1fa06b4
jeffpiazza <jeffpiazza@149d4557-f578-4cdc-88cc-a781409a2e9f>
2014-07-13 12:43:01 +0000

    MacDerbyReplay first commit

---
+++ trunk/replay/Source/AppDelegate.h
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.h
+//
+//  Created by Jeff Piazza on 4/21/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+}
+
+- (void) startRecording;
+- (void) stopRecording;
+- (void) replayRecording;
+
+@property BOOL isPlaying;
+
+@property (retain) NSArray* videoDevices;
+@property (retain) NSArray* audioDevices;
+@property (assign) AVCaptureDevice* selectedVideoDevice;
+@property (assign) AVCaptureDevice* selectedAudioDevice;
+
+@property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSView *previewView;
+
+@property (assign) IBOutlet NSWindow* urlSheet;
+@property (weak) IBOutlet NSTextField* urlField;
+
+// Playback:
+@property (weak) IBOutlet AVPlayerView *playerView;
+
+@property NSString* url;
+
+@end
---
+++ trunk/replay/Tests/MacDerbyReplayTests.m
@@ -0,0 +1,33 @@
+//
+//  MacDerbyReplayTests.m
+//
+//  Created by Jeff Piazza on 4/21/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MacDerbyReplayTests : XCTestCase
+
+@end
+
+@implementation MacDerbyReplayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
