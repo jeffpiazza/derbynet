2be8bd6d96aefbc7c25467883b3ac60ff7538f7e
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-07-18 02:01:54 +0000

    CocoaAsyncSocket command listener for Replay

--- replay/Source/AppDelegate.h
+++ replay/Source/AppDelegate.h
@@ -16,6 +16,9 @@
 - (void) stopRecording;
 - (void) replayRecording;
 
+- (void) setPortMessage: (NSString*) msg;
+- (void) setStatus: (NSString*) msg;
+
 @property BOOL isPlaying;
 
 @property (retain) NSArray* videoDevices;
@@ -25,14 +28,17 @@
 
 @property (assign) IBOutlet NSWindow *window;
 @property (weak) IBOutlet NSView *previewView;
+@property (weak) IBOutlet NSTextField* portView;
+@property (weak) IBOutlet NSTextField* statusView;
 
 @property (assign) IBOutlet NSWindow* urlSheet;
 @property (weak) IBOutlet NSTextField* urlField;
+
 @property (retain) Poller* poller;
 
 // Playback:
 @property (weak) IBOutlet AVPlayerView *playerView;
 
-@property NSString* url;
+@property (retain) NSString* url;
 
 @end
