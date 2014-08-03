523cf06f60b3cc92f36332cffd235e18fdee88f2
jeffpiazza <jeffpiazza@149d4557-f578-4cdc-88cc-a781409a2e9f>
2014-08-03 15:31:08 +0000

    Coordinator dashboard development - checkpoint

--- trunk/website/coordinator.php
+++ trunk/website/coordinator.php
@@ -3,9 +3,6 @@
 require_once('inc/data.inc');
 require_once('inc/authorize.inc');
 require_permission(SET_UP_PERMISSION);  // TODO: What's the correct permission?
-require_once('inc/timer-state.inc');
-require_once('inc/replay.inc');
-require_once('inc/kiosks.inc');
 ?>
 <html>
 <head>
@@ -30,127 +27,93 @@ $(document).bind("mobileinit", function() {
 <script type="text/javascript" src="js/coordinator.js"></script>
 </head>
 <body>
-<?php
-$banner_title = 'Race Coordinator Page';
-require('inc/banner.inc');
-?>
+<?php $banner_title = 'Race Dashboard'; require('inc/banner.inc'); ?>
+
+<div class="double_control_column">
+
+<div id="now-racing-group" class="scheduling_control_group">
+  <p>Waiting for coordinator-poll query...</p>
+</div>
+
+</div>
+
 <div class="control_column">
 
-
 <div class="control_group heat_control_group">
-  <h3>Sequence Control</h3>
-
-  <!-- TODO: Display current heat (round and heat); update -->
-  <!-- TODO: Select racing round explicitly -->
-
-  <!-- TODO: Start/stop racing: explicit am-racing state. -->
-
   <div class="centered_flipswitch">
-  <!-- label for="is-currently-racing">Now Racing</label -->
-  <input type="checkbox" data-role="flipswitch" name="is-currently-racing" id="is-currently-racing"
-    checked="checked"
-    data-on-text="Racing" data-off-text="Not Racing"/>
-<!-- onchange= -->
+    <input type="checkbox" data-role="flipswitch" name="is-currently-racing" id="is-currently-racing"
+      checked="checked"
+      onchange="handle_isracing_change();"
+      data-on-text="Racing" data-off-text="Not Racing"/>
   </div>
-
 
   <div class="block_buttons">
     <input type="button" data-enhanced="true" value="Skip Heat" onclick="handle_skip_heat()"/><br/>
     <input type="button" data-enhanced="true" value="Previous Heat" onclick="handle_previous_heat()"/><br/>
-    <!-- TODO: manual heat results -->
-    <input type="button" data-enhanced="true" value="Manual Results"/><br/>
+    <input type="button" data-enhanced="true" value="Manual Results" onclick="handle_manual_results()"/><br/>
+    <!-- TODO: discard results (one or all heats) -->
   </div>
 
 </div>
 
 
 <div class="control_group timer_control_group">
-  <!-- TODO: Show timer status -->
   <h3>Timer Status</h3>
-  <p><b><?php
-    $tstate = get_timer_state();
-    if ($tstate == TIMER_NOT_CONNECTED) {
-        echo 'NOT CONNECTED';
-    } else if ($tstate == TIMER_CONNECTED) {
-        echo 'CONNECTED ('.(time() - read_raceinfo('timer_last_contact')).' sec.)';
-    } else if ($tstate == TIMER_PREPARED) {
-        echo 'Prepared';
-    } else if ($tstate == TIMER_RUNNING) {
-        echo 'Race running';
-    } else {
-        echo 'Unknown ('.$tstate.')';
-    }
-?></b></p>
-  <p>The track has <?php echo get_lane_count(); ?> lane(s).</p>
-  <!-- TODO: Timer events:
-  Timer to database:
-      Hello/identity (type of timer, number of lanes)
-      Heartbeat
-      Gate opened
-      Race finished
-  DB to timer:
-      Prepare timer (with lane mask)
-      Number of lanes (configured by race coordinator)
-      Heat cancel/abort
-  -->
+  <p><b id="timer_status_text">Timer status not yet updated</b></p>
+  <p>The track has <span id="lane_count">an unknown number of</span> lane(s).</p>
 </div>
 
 <div class="control_group replay_control_group">
-  <!-- TODO: Show instant-replay status -->
-  <h3>Instant Replay Status</h3>
-  <p>Remote replay at <?= read_replay_host_and_port() ?></p>
-  <div class="block_buttons">
+  <h3>Replay Status</h3>
+  <p><b id="replay_status">Remote replay status not yet updated</b></p>
+  <div id="test_replay" class="block_buttons">
     <input type="button" data-enhanced="true" value="Test Replay" onclick="handle_test_replay();"/>
   </div>
 </div>
 
-
-
-<div class="kiosk_control_group">
-<div class="block_buttons">
-<?php
-try {
-    drop_old_kiosks();
-    $stmt = $db->query('SELECT address, name, page, last_contact FROM Kiosks ORDER BY name, last_contact');
-    foreach ($stmt as $row) {
-        echo '<div class="control_group kiosk_control">'."\n";
-        echo   '<p>Kiosk ';
-        echo   '<span class="kiosk_control_name">'.htmlspecialchars($row['name']).'</span> ';
-        echo   '<span class="kiosk_control_address">'.htmlspecialchars($row['address']).'</span> ';
-        echo   '</p>';
-        echo '<p class="last_contact">Last contact: '.$row['last_contact'].'</p>'."\n";
-
-        echo   '<label for="kiosk-page">Display:</label>'; // TODO: Fire action when selection changes.
-        echo   '<select name="kiosk-page" data-kiosk-address="'.htmlspecialchars($row['address']).'"'
-               .' onchange="handle_kiosk_page_change(this)"'
-               .'>'."\n";
-        echo   '<optgroup>'."\n";
-
-        scan_kiosk_pages('kiosks', $row['page']);
-        scan_kiosk_pages('local'.DIRECTORY_SEPARATOR.'kiosks', $row['page']);
-
-        echo   '</optgroup>'."\n";
-        echo   '</select>'."\n";
-        echo '</div>'."\n";
-    }
-} catch (PDOException $p) {
-    // TODO: No Kiosks table
-    echo '<h2>No Kiosks table defined.</h2>';  // TODO: Or other problem...
-    // TODO: What if not throwing exceptions?
-    // TODO: Create kiosk table on demand
-    // TODO: Allow assigning name to kiosk
-}
-?>
+<div id="kiosk_control_group" class="kiosk_control_group">
 </div>
-</div>
+
 </div>
 
 <div class="control_column">
 
-<div id="scheduling-control-group" class="scheduling_control_group">
-  <p>Waiting for coordinator-poll query...</p>
+<div id="ready-to-race-group" class="scheduling_control_group"></div>
+<div id="not-yet-scheduled-group" class="scheduling_control_group"></div>
+<div id="done-racing-group" class="scheduling_control_group"></div>
+
+<?php
+// TODO: Controls for creating 2nd round, grand finals round, including roster
+?>
 </div>
 
+<div id='modal_background'></div>
+
+<div id='kiosk_modal' class="hidden block_buttons">
+  <form>
+    <label for="kiosk_name_field">Name for kiosk:</label>
+    <input type="text" id="kiosk_name_field"/>
+    <input type="submit" data-enhanced="true" value="Assign"/>
+    <input type="button" data-enhanced="true" value="Cancel"
+      onclick='close_kiosk_modal();'/>
+  </form>
+</div>
+
+<div id='schedule_modal' class="hidden block_buttons">
+  <form>
+    <p>How many times should each racer appear in each lane?</p>
+    <select id="schedule_num_rounds">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+    </select>
+
+    <input type="submit" data-enhanced="true" value="Schedule"/>
+    <input type="button" data-enhanced="true" value="Cancel"
+      onclick='close_schedule_modal();'/>
+  </form>
 </div>
 
 </body>
--- trunk/website/kiosk.php
+++ trunk/website/kiosk.php
@@ -1,6 +1,11 @@
 <?php
 require('inc/data.inc');
-// page query argument to support testing
-$kiosk_page = isset($_GET['page']) ? $_GET['page'] : kiosk_page();
-require($kiosk_page);
+
+// 'page' query argument to support testing
+if (isset($_GET['page'])) {
+  require($_GET['page']);
+} else {
+  $kiosk_page = kiosk_page();
+  require($kiosk_page['page']);
+}
 ?>
