e799063a77c162431af6fb9c50cbc87d6045b4a4
jeffpiazza <jeffpiazza@users.noreply.github.com>
2014-11-26 02:22:41 +0000

    Bug fix: g_use_master_sched in racer-results.php

--- website/racer-results.php
+++ website/racer-results.php
@@ -3,6 +3,7 @@
 <?php
     require_once('inc/data.inc');
 
+    $use_master_sched = use_master_sched();
     $high_water_rounds = high_water_rounds();
 ?>
 <html>
@@ -13,7 +14,7 @@
     var g_last_update_time = ""; // First refresh is for everything
     var g_high_water_resultid = <?php echo high_water_resultid(); ?>;
     var g_high_water_group = <?php echo $high_water_rounds['roundid']; ?>;
-    var g_use_master_sched = <?php echo isset($use_master_sched) && $use_master_sched ? 1 : 0; ?>;
+    var g_use_master_sched = <?php echo $use_master_sched ? 1 : 0; ?>;
     var g_using_groupid = false;
 </script>
 <script type="text/javascript" src="js/update.js"></script>
