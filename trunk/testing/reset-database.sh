eebae1f6b56ca060bfc0a7e941c5b1b8f458df9d
jeffpiazza <jeffpiazza@149d4557-f578-4cdc-88cc-a781409a2e9f>
2015-01-11 14:21:57 +0000

    update-schema, and schema 2

--- trunk/testing/reset-database.sh
+++ trunk/testing/reset-database.sh
@@ -6,5 +6,6 @@ source `dirname $0`/common.sh
 `dirname $0`/login-coordinator.sh $BASE_URL
 
 curl_post action.php "action=run-sql&script=schema" | check_success
+curl_post action.php "action=run-sql&script=update-schema" | check_success
 
 `dirname $0`/import-roster.sh $BASE_URL
