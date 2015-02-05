36468fe1fc2bf4d559cae2158ccedccb5187e7b1
jeffpiazza <jeffpiazza@149d4557-f578-4cdc-88cc-a781409a2e9f>
2015-02-05 00:09:13 +0000

    Photo handling update: change photo URLs, tolerate unrecognized photos, prepare to handle car photos, too

--- trunk/website/photo.php
+++ trunk/website/photo.php
@@ -3,28 +3,65 @@ session_start();
 
 require_once('inc/photo-config.inc');
 
-// Path name is e.g. photo.php/thumb/mygreatphoto.jpg
-//  or photo.php/thumb/<cache-breaker>/mygreatphoto.jpg
+// URL for a file in one of the repositories is:
+//  photo.php/<repository>/file/<render>/<file-basename>/<cachebreaker>, e.g.
+//  photo.php/head        /file/thumb   /mygreatphoto.jpg/20150204133502
+//
+// URL for an arbitrary file named by a racer's database entry is:
+//  photo.php/<repository>/racer/<racerid>/<cachebreaker>
+// A racer-based URL requires another database fetch to get the file path, and
+// then returns exactly that file.
 
-$trailing = explode('/', $_SERVER['PATH_INFO']);
-$photo_render = $trailing[1];
-$breaker = count($trailing) > 2;
-$image_name = urldecode($trailing[count($trailing) - 1]);
+function parse_photo_url($url_path_info) {
+  $exploded = explode('/', $url_path_info);
+  if ($exploded[2] == 'file') {
+    return array('repository' => $exploded[1],
+                 'url_type' => $exploded[2], // 'file'
+                 'render' => $exploded[3],
+                 'basename' => urldecode($exploded[4]));
+  } else if ($exploded[2] == 'racer') {
+    return array('repository' => $exploded[1],
+                 'url_type' => $exploded[2], // 'racer'
+                 'racerid' => $exploded[3]);
+  } else {
+    return false;
+  }
+}
 
-$rr = $headshots->lookup($photo_render);
-if ($rr) {
-  $target_file_path = $rr->find_or_make_image_file($image_name);
+$parsed = parse_photo_url($_SERVER['PATH_INFO']);
+if (!$parsed) {  // Malformed URL
+  http_response_code(404);
+  exit(1);
+}
+
+$repo = photo_repository($parsed['repository']);
+if (!$repo) {  // No such repository
+  http_response_code(404);
+  exit(1);
+}
+
+if ($parsed['url_type'] == 'racer') {
+  $file_path = read_single_value('SELECT '.$repo->column_name()
+                                 .' FROM RegistrationInfo'
+                                 .' WHERE racerid = :racerid',
+                                 array(':racerid' => $parsed['racerid']));
+  if (!$file_path) {  // No such racer
+    http_response_code(404);
+    exit(1);
+  }
 } else {
-  echo '<h1>Unrecognized render: '.$photo_render.'</h1>'."\n";
-  exit(0); // $target_file_path = "";
+  $render = $repo->lookup($parsed['render']);
+  if (!$render) {  // No such render
+    http_response_code(404);
+    exit(1);
+  }
+  $file_path = $render->find_or_make_image_file($parsed['basename']);
 }
 
-if ($breaker) {
-  header('Pragma: public');
-  header('Cache-Control: max-age=86400, public');
-  header('Expires: '.gmdate('D, d M Y H:i:s', time() + 86400).' GMT');
-}
+header('Pragma: public');
+header('Cache-Control: max-age=86400, public');
+header('Expires: '.gmdate('D, d M Y H:i:s', time() + 86400).' GMT');
 
 header('Content-type: '.pseudo_mime_content_type($target_file_path));
 
-readfile($target_file_path);
+readfile($file_path);
