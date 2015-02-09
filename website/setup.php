b448b53e6c777bb241a7a91939227a0dc1e5cdb5
jeffpiazza <jeffpiazza@users.noreply.github.com>
2015-02-09 15:35:25 +0000

    Split off shared modal.js

--- website/setup.php
+++ website/setup.php
@@ -27,6 +27,7 @@ $(document).bind("mobileinit", function() {
 <!-- For flipswitch and select elements: -->
 <script type="text/javascript" src="js/jquery.mobile-1.4.2.min.js"></script>
 <script type="text/javascript" src="js/dashboard-ajax.js"></script>
+<script type="text/javascript" src="js/modal.js"></script>
 <script type="text/javascript" src="js/setup.js"></script>
 </head>
 <body>
