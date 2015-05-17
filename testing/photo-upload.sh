199f8e0f1d1185a6dc81d36d7e16a5a2cdf33e20
jeffpiazza <jeffpiazza@users.noreply.github.com>
2015-05-17 19:07:29 +0000

    Clean up photo testing

--- testing/photo-upload.sh
+++ testing/photo-upload.sh
@@ -10,5 +10,5 @@ if [ "$2" = "" -o "$4" != "" ]; then
 fi
 
 curl --location -s -b $COOKIES_CURL -c $COOKIES_CURL $BASE_URL/action.php \
-    -X POST -F photo="@$2" -F action=photo.upload -F racerid="$3"
+    -X POST -F photo="@$2" -F action=photo.upload -F racerid="$3" | check_success
 
