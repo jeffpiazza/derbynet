ef4bae55b61f8d4b0be774bcd28881b80fc3e5dc
Jeff Piazza <jeff@jeffpiazza.org>
2017-01-12 09:12:35 -0500

    Update Mac installer to create or update php.ini file.

---
+++ installer/mac/scripts/webserver/create-or-update-php-ini.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+# If /etc/php.ini is absent, manufacture one from /etc/php.ini.default
+
+ORIGINAL_INI=/etc/php.ini.default
+
+if [ -f /etc/php.ini ] ; then
+    mv /etc/php.ini /etc/php.ini.pre-derbynet
+    ORIGINAL_INI=/etc/php.ini.pre-derbynet
+fi
+
+touch     /var/log/php_errors
+chmod 777 /var/log/php_errors
+
+TIMEZONE=`systemsetup -gettimezone | sed -e "s/Time Zone: //"`
+
+sed -e "s#^upload_max_filesize = 2M#upload_max_filesize = 8M#" \
+    -e "s#^;date.timezone =#date.timezone = $TIMEZONE#" \
+    -e "s#^;error_log = syslog#error_log = /var/log/php_errors# \
+    < $ORIGINAL_INI > /etc/php.ini
+
+# Apache needs to be restarted for these changes to take effect; that happens in 
+# the update-apache-config.sh script.
