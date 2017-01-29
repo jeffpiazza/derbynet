a39aff747ac41bad97953325cf464c22dd4fb891
Jeff Piazza <jeff@jeffpiazza.org>
2017-01-28 19:46:50 -0500

    Fix 'chown' for ~/Public/DerbyNet folder.  Yikes. Add description for "mac.dist" target in build file.

--- installer/mac/scripts/webserver/maybe-create-local-directory.sh
+++ installer/mac/scripts/webserver/maybe-create-local-directory.sh
@@ -3,7 +3,7 @@
 mkdir -m 777 /Library/WebServer/Documents/derbynet/local
 
 mkdir -m 775 "$HOME/Public/DerbyNet"
-chown $USER _www "$HOME/Public/DerbyNet"
+chown $USER:_www "$HOME/Public/DerbyNet"
 
 cat <<EOF > /Library/WebServer/Documents/derbynet/local/default-file-path.inc
 <?php
