36384e8abd03f9f33cb3e490c6f6042f822740ae
Jeff Piazza <jeff@jeffpiazza.org>
2017-02-20 10:03:42 -0500

    Revert "Timer bug fixes: Don't create multiple logwriters, and use three-digit format for milliseconds."
    This reverts commit 7ab86f0e6d685e4cf4ab112372c5c9402d4730d2.
    TimerMain includes playback-related changes that aren't yet checked in.

--- timer/src/org/jeffpiazza/derby/Timestamp.java
+++ timer/src/org/jeffpiazza/derby/Timestamp.java
@@ -7,7 +7,7 @@ public class Timestamp {
   private static final SimpleDateFormat yyyy_mm_dd_hh_mm_ss
       = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
   private static final SimpleDateFormat hh_mm_ss
-      = new SimpleDateFormat("HH:mm:ss.SSS");
+      = new SimpleDateFormat("HH:mm:ss.S");
 
   public static String string() {
     // return yyyy_mm_dd_hh_mm_ss.format(Calendar.getInstance().getTime());
