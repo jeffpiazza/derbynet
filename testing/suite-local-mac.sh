de9af0ea2bc42f23a9e0b3a498f7f874f2e3fa91
Jeff Piazza <jeff@jeffpiazza.org>
2018-01-14 09:08:44 -0500

    Add test coverage for ab-initio setup.

--- testing/suite-local-mac.sh
+++ testing/suite-local-mac.sh
@@ -4,5 +4,6 @@ BASE_URL=$1
 set -e -E -o pipefail
 source `dirname $0`/common.sh
 
+`dirname $0`/test-ab-initio-setup.sh "$BASE_URL"
 `dirname $0`/exercise-one-database.sh "$BASE_URL" mysql
 `dirname $0`/exercise-one-database.sh "$BASE_URL" sqlite
