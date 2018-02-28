a5fa582ddb1b7de4cf058c1f4da00b7edc4d0726
Jeff Piazza <jeff@jeffpiazza.org>
2018-02-28 18:46:05 -0500

    Reorganize directory structure for printables. Add support for printing racer printables by car number alone, without racers having to be entered in the database.

---
+++ website/print.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+if (isset($_SERVER['PATH_INFO'])) {
+  $path_info = $_SERVER['PATH_INFO'];
+} else if (isset($_SERVER['ORIG_PATH_INFO'])) {
+  // TODO Obsolete?
+  // Rewrite rules in Apache 2.2 may leave ORIG_PATH_INFO instead of PATH_INFO
+  $path_info = 'print.php'.$_SERVER['ORIG_PATH_INFO'];
+} else {
+  echo "Debugging \$_SERVER:\n";
+  var_export($_SERVER);
+  exit(0);
+}
+
+$exploded = explode('/', $path_info);
+while ($exploded[0] == '') {
+  array_shift($exploded);
+}
+$inc = array_shift($exploded);
+
+function document_class() {
+  global $exploded;
+  return $exploded[0];
+}
+
+function new_document() {
+  $doc_class = document_class();
+  return new $doc_class;
+}
+
+require_once('print/'.$inc.'.inc');
+?>
