0fbeac79704eeddff18a2ae2ac099c3d7e5417ac
Jeff Piazza <jeff@jeffpiazza.org>
2018-05-20 14:03:41 -0400

    Migrate all-pages-test to puppeteer. Remove phantomjs tests.

---
+++ testing/puppeteer/all-pages-test.js
@@ -0,0 +1,83 @@
+const assert = require("./assert.js");
+
+const puppeteer = require('puppeteer');
+
+var root = 'http://localhost-will-fail/derbynet';
+if (process.argv.length > 2) {
+  root = process.argv[2];
+}
+if (root.substring(0, 'http://'.length) != 'http://') {
+  root = 'http://' + root;
+}
+if (root.substring(root.length - 1) == '/') {
+  root = root.substring(0, root.length - 1);
+}
+
+var debugging = false;
+
+var role = 'RaceCoordinator';
+var password = 'doyourbest';
+
+
+var all_uris = [
+  "about.php",
+  "awards-editor.php",
+  "awards-presentation.php",
+  "checkin.php",
+  "class-editor.php",
+  "coordinator.php",
+  // "export.php",
+  "import-awards.php",
+  "import-results.php",
+  "import-roster.php",
+  "index.php",
+  "judging.php",
+  "kiosk-dashboard.php",
+  "login.php",
+  "ondeck.php",
+  "photo-thumbs.php",
+  "racer-results.php",
+  "settings.php",
+  "setup.php",
+  "standings.php"
+];
+
+puppeteer.launch({devtools: debugging}).then(async browser => {
+  const all_pages = await browser.pages();
+  const page = all_pages[0];
+  page.setViewport({width: 1200,
+                    height: 1800});
+  page.on('pageerror', (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  page.on('error', (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  for (u in all_uris) {
+    var uri = all_uris[u];
+    console.log("  " + "  " + "  " + uri);
+    await page.goto(root + '/' + uri);
+  }
+
+  console.log("");
+  console.log("  " + "  " + "  Logging in");
+  await page.goto(root + '/login.php');
+  await page.evaluate(function (role, password) {
+    window.handle_login(role, password);
+  }, role, password);
+  await page.waitForNavigation();
+  console.log("");
+
+  for (u in all_uris) {
+    var uri = all_uris[u];
+    console.log("  " + "  " + "  " + uri);
+    await page.goto(root + '/' + uri);
+  }
+  
+  if (!debugging) {
+    await browser.close();
+  }
+});
--- testing/test-basic-javascript.sh
+++ testing/test-basic-javascript.sh
@@ -4,24 +4,14 @@ BASE_URL="$1"
 
 set -e -E -o pipefail
 
-PHANTOMJS="$(which phantomjs)"
-if [ ! -x "$PHANTOMJS" ] ; then
-    echo Skipping phantomjs tests
-    exit
-fi
-
-echo " " " " " " test-basic-javascript
-
-echo "[]" > "`dirname $0`/cookies.phantomjs"
-phantomjs "`dirname $0`/phantomjs/test-all-pages.js" "$BASE_URL"
-phantomjs "`dirname $0`/phantomjs/login.js" "$BASE_URL"
-phantomjs "`dirname $0`/phantomjs/test-all-pages.js" "$BASE_URL"
-
 # Puppeteer tests
 # https://github.com/GoogleChrome/puppeteer
+
+echo " " " " " " all-pages-test.js
+node "`dirname $0`/puppeteer/all-pages-test.js" "$BASE_URL"
 echo " " " " " " coordinator-test.js
 node "`dirname $0`/puppeteer/coordinator-test.js" "$BASE_URL"
 echo " " " " " " ondeck-columns-test.js
 node "`dirname $0`/puppeteer/ondeck-columns-test.js" "$BASE_URL"
 
-echo " " " " " " Done with test-basic-javascript
+
