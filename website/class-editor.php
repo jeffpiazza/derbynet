5f8362c62ce71ed17c978888d4fc8d9661bdbebc
Jeff Piazza <jeff@jeffpiazza.org>
2018-08-12 10:13:55 -0400

    Add, delete, re-order, and edit ranks in each class.

--- website/class-editor.php
+++ website/class-editor.php
@@ -21,9 +21,12 @@ require_permission(SET_UP_PERMISSION);
 <script type="text/javascript" src="js/dashboard-ajax.js"></script>
 <script type="text/javascript" src="js/class-editor.js"></script>
 <script type="text/javascript">
+function use_subgroups() { return <?php echo json_encode(read_raceinfo_boolean('use-subgroups')); ?>; }
 function group_label() { return <?php echo json_encode(group_label()); ?>; }
 function group_label_lc() { return <?php echo json_encode(group_label_lc()); ?>; }
-$(function() { show_class_editor_modal(); });
+function subgroup_label() { return <?php echo json_encode(subgroup_label()); ?>; }
+function subgroup_label_lc() { return <?php echo json_encode(subgroup_label_lc()); ?>; }
+$(function() { show_edit_all_classes_modal(); });
 </script>
 </head>
 <body>
@@ -32,11 +35,8 @@ $(function() { show_class_editor_modal(); });
   // really have much effect.
 make_banner(group_label().' Editor', 'setup.php'); ?>
 
-<div id="class_editor_modal" class="modal_dialog hidden block_buttons">
+<div id="edit_all_classes_modal" class="modal_dialog hidden block_buttons">
   <form>
-    <input type="button" value="Add <?php echo group_label(); ?>" data-enhanced="true"
-           onclick="show_add_class_modal();" />
-
     <h3>Drag to Re-order <?php echo group_label(); ?>s</h3>
 
     <div id="groups_container">
@@ -46,8 +46,13 @@ make_banner(group_label().' Editor', 'setup.php'); ?>
 
     <br/>
 
+    <input type="button" value="Add <?php echo group_label(); ?>" data-enhanced="true"
+           onclick="show_add_class_modal();" />
+
+    <br/>
+
     <input type="button" value="Close" data-enhanced="true"
-           onclick="close_class_editor_modal();"/>
+           onclick="close_edit_all_classes_modal();"/>
   </form>
 </div>
 
@@ -74,14 +79,54 @@ make_banner(group_label().' Editor', 'setup.php'); ?>
       <p><span id="completed_rounds_count"></span> completed round(s) exist for this class.</p>
     </div>
 
+    <div id="edit_ranks_extension" class="hidden">
+      <h3>Drag to Re-order <?php echo subgroup_label(); ?>s</h3>
+      <div id="ranks_container">
+      </div>
+      <br/>
+      <input type="button" value="Add <?php echo subgroup_label(); ?>" data-enhanced="true"
+             onclick="show_add_rank_modal();" />
+      <br/>
+    </div>
+
     <input type="submit" data-enhanced="true"/>
     <input type="button" value="Cancel" data-enhanced="true"
            onclick="close_edit_one_class_modal();"/>
 
-    <div id="delete_button_extension">
+    <div id="delete_class_extension">
     <input type="button" value="Delete <?php echo group_label(); ?>"
            class="delete_button" data-enhanced="true"
            onclick="handle_delete_class();"/>
+    </div>
+  </form>
+</div>
+
+<div id="add_rank_modal" class="modal_dialog hidden block_buttons">
+  <h3>Add New <?php echo subgroup_label(); ?></h3>
+  <form>
+    <input type="hidden" name="action" value="rank.add"/>
+    <input type="hidden" name="classid"/>
+    <input name="name" type="text"/>
+
+    <input type="submit" data-enhanced="true"/>
+    <input type="button" value="Cancel" data-enhanced="true"
+           onclick="close_add_class_modal();"/>
+  </form>
+</div>
+
+<div id="edit_one_rank_modal" class="modal_dialog hidden block_buttons">
+  <h3>New <?php echo subgroup_label(); ?> Name</h3>
+  <form>
+    <input id="edit_rank_name" name="name" type="text"/>
+
+    <input type="submit" data-enhanced="true"/>
+    <input type="button" value="Cancel" data-enhanced="true"
+           onclick="close_edit_one_rank_modal();"/>
+
+    <div id="delete_rank_extension">
+    <input type="button" value="Delete <?php echo subgroup_label(); ?>"
+           class="delete_button" data-enhanced="true"
+           onclick="handle_delete_rank();"/>
     </div>
   </form>
 </div>
--- website/js/class-editor.js
+++ website/js/class-editor.js
@@ -1,17 +1,17 @@
 // Requires dashboard-ajax.js
 
-// Note that close_class_editor_modal returns to setup.php upon dismissal of the
+// Note that close_edit_all_classes_modal returns to setup.php upon dismissal of the
 // modal dialog.
 
-function show_class_editor_modal() {
+function show_edit_all_classes_modal() {
     reload_class_list();
-    show_modal("#class_editor_modal", function () { 
+    show_modal("#edit_all_classes_modal", function () { 
         return false;
     });
 }
 
-function close_class_editor_modal() {
-    close_modal("#class_editor_modal");
+function close_edit_all_classes_modal() {
+    close_modal("#edit_all_classes_modal");
     window.location='setup.php';
 }
 
@@ -33,20 +33,34 @@ function close_add_class_modal() {
     close_secondary_modal("#add_class_modal");
 }
 
+// Show only the ul of ranks for this class, and only if we're using ranks in
+// the first place.
+function hide_ranks_except(classid) {
+  $("#ranks_container ul").addClass("hidden");
+  $("#ranks_container ul[data-classid=" + classid + "]").removeClass("hidden");
+}
+
 function edit_one_class(who) {
   var list_item = $(who).parent("li");
+  var classid = list_item.attr('data-classid');
+
+  // list_item is an <li> from #edit_all_classes_modal.  We expect it to have a
+  // .class-name span child, plus data attributes for classid, count, and nrounds.
+  
   $("#edit_class_name").val(list_item.find('.class-name').text());
-  $("#edit_class_name").data('classid', list_item.data('classid'));
+  $("#edit_class_name").attr('data-classid', classid);
+
+  hide_ranks_except(classid);
 
   // The "completed rounds" message appears only if there are no native racers,
   // but there are some completed rounds that prevent offering deletion of the
   // class.
-  $("#completed_rounds_extension").toggleClass('hidden',
-                                               !(list_item.data('count') == 0 && list_item.data('nrounds') != 0));
-  $("#completed_rounds_count").text(list_item.data('nrounds'));
+  var count = list_item.attr('data-count');
+  var nrounds = list_item.attr('data-nrounds');
+  $("#completed_rounds_extension").toggleClass('hidden', !(count == 0 && nrounds != 0));
+  $("#completed_rounds_count").text(nrounds);
 
-  $("#delete_button_extension").toggleClass('hidden',
-                                            !(list_item.data('count') == 0 && list_item.data('nrounds') == 0));
+  $("#delete_class_extension").toggleClass('hidden', !(count == 0 && nrounds == 0));
   show_edit_one_class_modal(list_item);
 }
 
@@ -55,7 +69,7 @@ function show_edit_one_class_modal(list_item) {
         $.ajax(g_action_url,
                {type: 'POST',
                 data: {action: 'class.edit',
-                       classid: list_item.data('classid'),
+                       classid: list_item.attr('data-classid'),
                        name: $("#edit_class_name").val()},
                 success: function () {
                     reload_class_list();
@@ -77,10 +91,84 @@ function handle_delete_class() {
     $.ajax(g_action_url,
            {type: 'POST',
             data: {action: 'class.delete',
-                   classid: $("#edit_class_name").data('classid')
+                   classid: $("#edit_class_name").attr('data-classid')
                   },
             success: function(data) {
               repopulate_class_list(data);
+            }
+           });
+  }
+  return false;
+}
+
+function show_add_rank_modal() {
+  var classid = $("#edit_class_name").attr('data-classid');
+  $("#add_rank_modal input[name='classid']").val(classid);
+  show_tertiary_modal("#add_rank_modal", function() {
+    close_add_rank_modal();
+    $.ajax(g_action_url,
+           {type: 'POST',
+            data: $("#add_rank_modal form").serialize(),
+            success: function(data) {
+              repopulate_class_list(data);
+              hide_ranks_except(classid);
+            }});
+    return false;
+  });
+}
+
+function close_add_rank_modal() {
+  close_tertiary_modal("#add_rank_modal");
+}
+
+function edit_one_rank(who) {
+  var list_item = $(who).parent("li");
+  $("#edit_rank_name").val(list_item.find('.rank-name').text());
+  $("#edit_rank_name").attr('data-rankid', list_item.attr('data-rankid'));
+
+  // True if we're the only rank for this class:
+  var only_rank = list_item.parent("ul").find("li").length == 1;
+  var count = list_item.attr('data-count');
+  $("#delete_rank_extension").toggleClass('hidden', only_rank || (count > 0));
+  show_edit_one_rank_modal(list_item);
+}
+
+function show_edit_one_rank_modal(list_item) {
+  var classid = list_item.closest("ul").attr('data-classid');
+  $("#edit_rank_name").attr('data-classid', classid);
+  show_tertiary_modal("#edit_one_rank_modal", function() {
+    $.ajax(g_action_url,
+           {type: 'POST',
+            data: {action: 'rank.edit',
+                   rankid: list_item.attr('data-rankid'),
+                   name: $("#edit_rank_name").val()},
+            success: function(data) {
+              repopulate_class_list(data);
+              hide_ranks_except(classid);
+            }});
+
+    close_edit_one_rank_modal();
+    return false;
+  });
+}
+
+function close_edit_one_rank_modal() {
+  close_tertiary_modal("#edit_one_rank_modal");
+}
+
+function handle_delete_rank() {
+  var classid = $("#edit_rank_name").attr('data-classid');
+  close_edit_one_rank_modal();
+  if (confirm('Really delete ' + subgroup_label_lc()
+              + ' "' + $("#edit_rank_name").val() + '"?')) {
+    $.ajax(g_action_url,
+           {type: 'POST',
+            data: {action: 'rank.delete',
+                   rankid: $("#edit_rank_name").attr('data-rankid')
+                  },
+            success: function(data) {
+              repopulate_class_list(data);
+              hide_ranks_except(classid);
             }
            });
   }
@@ -96,32 +184,73 @@ function reload_class_list() {
             }});
 }
 
-function repopulate_class_list(data) {
-    var classes = data.getElementsByTagName("class");
-    if (classes.length > 0) {
-        $("#groups").empty();
-        for (var i = 0; i < classes.length; ++i) {
-            var cl = classes[i];
-            $("#groups").append(
-              "<li class='ui-li-has-alt'"
-                    + " data-classid='" + cl.getAttribute('classid') + "'"
-                    + " data-count='" + cl.getAttribute('count') + "'"
-                    + " data-nrounds='" + cl.getAttribute('nrounds') + "'"
+// For XML <class> element cl, populate a list of ranks within the class in the
+// edit_one_class modal.
+function populate_rank_list(cl) {
+  $("#edit_ranks_extension").removeClass('hidden');
+  var classid = cl.getAttribute('classid');
+  var rank_ul = $("<ul data-role='listview' data-split-icon='gear'></ul>").appendTo("#ranks_container");
+  rank_ul.attr('data-classid', cl.getAttribute('classid'));
+
+  var ranks = cl.getElementsByTagName("rank");
+  for (var ri = 0; ri < ranks.length; ++ri) {
+    var rank = ranks[ri];
+    var rank_li = $("<li class='ui-li-has-alt'"
+                    + " data-rankid='" + rank.getAttribute('rankid') + "'"
+                    + " data-count='" + rank.getAttribute('count') + "'"
                     + ">"
-                    + "<p><span class='class-name'></span><span class='count'></span></p>"
-                    + "<a class='ui-btn ui-btn-icon-notext ui-icon-gear' onclick='edit_one_class(this);'></a>"
-                    + "</li>\n");
-            $("#groups li:last p .class-name").text(cl.getAttribute('name'));
-            $("#groups li:last p .count").text("(" + cl.getAttribute('count') + ")");
-        }
+                    + "<p class='rank'></p>"
+                    + "<a class='ui-btn ui-btn-icon-notext ui-icon-gear' onclick='edit_one_rank(this);'></a>"
+                    + "</li>").appendTo(rank_ul);
+    var rank_p = rank_li.find("p");
+    $("<span class='rank-name'></span>").text(rank.getAttribute('name')).appendTo(rank_p);
+    $("<span class='count'></span>").text("(" + rank.getAttribute('count') + ")").appendTo(rank_p);
+  }
+  rank_ul.listview().listview("refresh");
+
+  rank_ul.sortable({stop: function(event, ui) {
+      var data = {action: 'rank.order'};
+      $("#ranks_container ul[data-classid=" + classid + "] li").each(function(i) {
+        data['rankid_' + (i + 1)] = $(this).attr('data-rankid');
+      });
+      $.ajax(g_action_url,
+             {type: 'POST',
+              data: data});
+  }});
+}
+
+function repopulate_class_list(data) {
+  var classes = data.getElementsByTagName("class");
+  if (classes.length > 0) {
+    $("#ranks_container").empty();
+    $("#groups").empty();
+    for (var i = 0; i < classes.length; ++i) {
+      var cl = classes[i];
+
+      var group_li = $("<li class='ui-li-has-alt'"
+                       + " data-classid='" + cl.getAttribute('classid') + "'"
+                       + " data-count='" + cl.getAttribute('count') + "'"
+                       + " data-nrounds='" + cl.getAttribute('nrounds') + "'"
+                       + ">"
+                       + "<p></p>"
+                       + "<a class='ui-btn ui-btn-icon-notext ui-icon-gear' onclick='edit_one_class(this);'></a>"
+                       + "</li>").appendTo("#groups");
+      var group_p = group_li.find("p");
+      $("<span class='class-name'></span>").text(cl.getAttribute('name')).appendTo(group_p);
+      $("<span class='count'></span>").text("(" + cl.getAttribute('count') + ")").appendTo(group_p);
+
+      if (use_subgroups()) {
+        populate_rank_list(cl);
+      }
     }
+  }
 }
 
 $(function () {
     $("#groups").sortable({stop: function(event, ui) {
       var data = {action: 'class.order'};
-      $("#class_editor_modal ul li").each(function(i) {
-        data['classid_' + (i + 1)] = $(this).data('classid');
+      $("#edit_all_classes_modal ul li").each(function(i) {
+        data['classid_' + (i + 1)] = $(this).attr('data-classid');
       });
       
       $.ajax(g_action_url,
