9fbe8e5f24692677950b5bde36a9db64fa62242d
Jeff Piazza <jeff@jeffpiazza.org>
2018-09-03 07:52:32 -0400

    Integrate SheetsJS for reading full spreadsheets (not just CSV files) directly. Repair corrupted sample-names.csv file.

--- website/js/import-csv.js
+++ website/js/import-csv.js
@@ -18,17 +18,59 @@ function onDrop(draggable, droppable) {
 function onUndrop(draggable) {
 }
 
+var g_filename;
+var g_file_contents;
+var g_workbook;
+
 /////////////////////////////////////////////////////////////////////////////////////////
 // Populate the page's main table with the data from the csv.
 /////////////////////////////////////////////////////////////////////////////////////////
 function onFileSelect(event) {
-  var file = event.target.files[0];
-  
-  loadCsvContentTable(file, $('input:radio[name="encoding-select"]:checked').val(),
-                      usingLabelTargets());
+  g_filename = event.target.files[0];
+
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+    g_file_contents = e.target.result;
+    loadWorkbook(0);
+  };
+
+  reader.onerror = function() {
+    alert('Unable to read ' + g_filename.fileName);
+  };
+
+  reader.readAsBinaryString(g_filename);
+}
+
+// With file already read into g_file_contents
+function loadWorkbook(sheet_index) {
+  var codepage = $('#select-encoding :selected').val();
+  try {
+    // For CSV files, XLSX doesn't seem to handle code page choices correctly.
+    // Explicitly decoding the file content into a string seems to give better
+    // results.
+    var translated = cptable.utils.decode(codepage, g_file_contents);
+    g_workbook = XLSX.read(translated, {type: 'string'});
+  } catch (error) {
+    // ... but for xlsx workbooks, the translated string looks like a corrupted
+    // zip file, so use the untranslated file content as a binary string.
+    g_workbook = XLSX.read(g_file_contents, {type: 'binary',
+                                             codepage: codepage});
+  }
+
+  if (g_workbook) {
+    if (sheet_index >= g_workbook.SheetNames.length) {
+      sheet_index = 0;
+    }
+    buildWorksheetSelector(sheet_index);
+    loadContentFromSheet(g_workbook.SheetNames[sheet_index]);
+  }
+}
+
+function loadContentFromSheet(sheet_name) {
+  populateContentTable(sheet_name);
 
   $(".file_target").addClass("hidden");
-  $("#encoding").addClass("hidden");
 
   $(".fields").removeClass("hidden");
   $("#import_button_div").removeClass("hidden");
@@ -39,67 +81,147 @@ function onFileSelect(event) {
   // don't appear immediately within the DOM.  Delaying like this seems to work,
   // but isn't efficient or guaranteed reliable.
   setTimeout(function() {
-    onFileContentLoaded(file);
+    onFileContentLoaded(g_filename);
     makeDroppableLabelTarget($(".label_target"));
   }, 100);
 }
 
-// Reads 'file' as text in 'encoding', populating the table named csv_content.
-// If 'add_label_targets' is true, then an initial row of label_target <th> elements is
-// written to the table.
-function loadCsvContentTable(file, encoding, add_label_targets) {
-  var reader = new FileReader();
+function populateContentTable(sheetname) {
+  var csv_content = $("#csv_content");
+  csv_content.empty();
 
-  reader.onload = function(event) {
-    var csv = event.target.result;
-    var csv_content = $("#csv_content");
-    try {
-      var data = $.csv.toArrays(csv);
-      var html = '';
-      var first = true;
-      for (var row in data) {
-        if (first && add_label_targets) {
-          // Add an extra row of label targets on top
-          var target_row = $('<tr id="label_target_row"/>').appendTo(csv_content);
-          target_row.append('<th/>');
-          for (var item in data[row]) {
-            $('<th class="label_target"><p>(Drag Label Here)</p></th>').attr("data-column", item)
-              .appendTo(target_row);
-          }
-        }
-        var table_row = $('<tr/>').attr("data-row", 1 + parseInt(row)).appendTo(csv_content);
-        if (first) {
-          table_row.addClass("header_row");
-        }
+  var sheet = g_workbook.Sheets[sheetname];
 
-        // First column shows the outcome of each row's upload attempt.
-        // First row also includes flipswitch for header row.
-        if (first) {
-          $('<th/>').append(
-            '<label for="header-row-present">Header row?</label>',
-            '<input type="checkbox" name="header-row-present" id="header-row-present"' +
-              ' data-role="flipswitch" checked="checked"/>')
-            .appendTo(table_row)
-            .trigger("create");
-          table_row.find('input[type="checkbox"]').on("change", handleHeaderRowPresentChange);
-        } else {
-          table_row.append('<th class="outcome"/>');
-        }
-        for (var item in data[row]) {
-          $('<td class="dim"/>').addClass('column' + item).text(data[row][item]).appendTo(table_row);
-        }
-        first = false;
+  var arrays = XLSX.utils.sheet_to_json(sheet, {header: 1});
+
+  var first = true;
+  for (var row in arrays) {
+    if (first && usingLabelTargets()) {
+      // Add an extra row of label targets on top
+      var target_row = $('<tr id="label_target_row"/>').appendTo(csv_content);
+      target_row.append('<th/>');
+      for (var item in arrays[row]) {
+        $('<th class="label_target"><p>(Drag Label Here)</p></th>').attr("data-column", item)
+          .appendTo(target_row);
       }
-    } catch(err) {
-      alert('Failure: ' + err);
     }
-  };
+    var table_row = $('<tr/>').attr("data-row", 1 + parseInt(row)).appendTo(csv_content);
+    if (first) {
+      table_row.addClass("header_row");
+    }
 
-  reader.onerror = function() {
-    alert('Unable to read ' + file.fileName);
-  };
+    // First column shows the outcome of each row's upload attempt.
+    // First row also includes flipswitch for header row.
+    if (first) {
+      $('<th/>').append(
+        '<label for="header-row-present">Header row?</label>',
+        '<input type="checkbox" name="header-row-present" id="header-row-present"' +
+          ' data-role="flipswitch" checked="checked"/>')
+        .appendTo(table_row)
+        .trigger("create");
+      table_row.find('input[type="checkbox"]').on("change", handleHeaderRowPresentChange);
+    } else {
+      table_row.append('<th class="outcome"/>');
+    }
+    for (var item in arrays[row]) {
+      $('<td class="dim"/>').addClass('column' + item).text(arrays[row][item]).appendTo(table_row);
+    }
+    first = false;
+  }
+}
 
-  reader.readAsText(file, encoding);
+/////////////////////////////////////////////////////////////////////////////////////////
+// Selector for encodings
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function buildEncodingSelector() {
+  var codePageNames = [
+    {codepage: '1252', name: 'Western (Windows-1252, ISO-8859-1)'},
+    {codepage: '10000', name: 'Mac OS Roman (Macintosh)'},
+    {codepage: '65001', name: 'UTF-8'},
+    {},
+    {codepage: '437', name: 'DOS Original'},
+    {codepage: '850', name: 'DOS Latin-1'},
+    {codepage: '895', name: 'DOS Czech'},
+    {codepage: '620', name: 'DOS Polish'},
+    {codepage: '737', name: 'DOS Greek'},
+    {codepage: '852', name: 'DOS Latin-2'},
+    {codepage: '857', name: 'DOS Latin-5'},
+    {codepage: '861', name: 'DOS Icelandic'},
+    {codepage: '865', name: 'DOS Danish/Norwegian'},
+    {codepage: '866', name: 'DOS Belarusian/Russian/Ukranian'},
+    {codepage: '874', name: 'Windows Thai'},
+    {codepage: '932', name: 'Windows Shift-JIS (Windows-932)'},
+    {codepage: '936', name: 'Windows Simplified Chinese (Windows-936)'},
+    {codepage: '949', name: 'Windows Korean (Windows-949)'},
+    {codepage: '950', name: 'Windows Traditional Chinese (Windows-950)'},
+    {codepage: '1250', name: 'Windows Central Europe (Windows-1250)'},
+    {codepage: '1251', name: 'Windows Cyrillic (Windows-1251)'},
+    {codepage: '1253', name: 'Windows Greek (Windows-1253)'},
+    {codepage: '1254', name: 'Windows Turkish (Windows-1254)'},
+    {codepage: '1255', name: 'Windows Hebrew (Windows-1255)'},
+    {codepage: '1256', name: 'Windows Arabic (Windows-1256)'},
+    {codepage: '1257', name: 'Windows Baltic (Windows-1257)'},
+    {codepage: '1258', name: 'Windows Vietnamese (Windows-1258)'},
+    {codepage: '10006', name: 'Macintosh Greek'},
+    {codepage: '10007', name: 'Macintosh Cyrillic'},
+    {codepage: '10008', name: 'Macintosh Simplified Chinese'},
+    {codepage: '10029', name: 'Macintosh Central Europe'},
+    {codepage: '10079', name: 'Macintosh Icelandic'},
+    {codepage: '10081', name: 'Macintosh Turkish'},
+  ];
+
+  var encoding = $("#encoding");
+  encoding.append("<label for='select-encoding'>Encoding:</label>");
+  var select = $("<select id='select-encoding'></select>").appendTo(encoding);
+
+  for (var cpi in codePageNames) {
+    var cp = codePageNames[cpi];
+    if (cp.hasOwnProperty('codepage')) {
+      select.append("<option value=\"" + cp.codepage + "\">" + cp.name + "</option>");
+    } else {
+      select.append("<option value=\"\" disabled></option>");
+    }
+  }
+  select.val("65001");  // UTF-8
+  select.on('change', onEncodingChange);
+}
+
+function onEncodingChange() {
+  if (g_file_contents) {
+    var sheet_index = 0;
+    if ($("#select-sheet").length) {
+      sheet_index = $("#select-sheet :selected").val();
+    }
+    loadWorkbook(sheet_index);
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Selector for worksheets
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function buildWorksheetSelector(sheet_index) {
+  var select = $("#select-sheet");
+  if (select.length == 0) {
+    var sheet = $("#sheet");
+    sheet.append("<label for='select-sheet'>Sheet:</label>");
+    select = $("<select id='select-sheet'></select>").appendTo(sheet);
+    select.off('change');
+  }
+
+  select.empty();
+  for (var i in g_workbook.SheetNames) {
+    var option = $("<option value=\"" + i + "\"/>").appendTo(select);
+    option.text(g_workbook.SheetNames[i]);
+  }
+  select.val(sheet_index);
+
+  select.on('change', onWorksheetChange);
+}
+
+function onWorksheetChange() {
+  loadContentFromSheet(g_workbook.SheetNames[$("#select-sheet :selected").val()]);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -235,9 +357,11 @@ function collectDraggedLabels() {
 // parameter_names is an array mapping column number to parameter name for the
 // action.  Typically parameter_names is computed by collectDraggedLabels,
 // above.
-function uploadTableRowsFrom(row, action, parameter_names) {
+//
+// failures is the count of row import failures so far
+function uploadTableRowsFrom(row, action, parameter_names, failures) {
   if ($('[data-row="' + row + '"]').length == 0) {
-    onUploadComplete();
+    onUploadComplete(failures);
     return;
   }
 
@@ -257,7 +381,7 @@ function uploadTableRowsFrom(row, action, parameter_names) {
 
   if (allblank) {
     $('[data-row="' + row + '"] th').text('SKIP');
-    uploadTableRowsFrom(row + 1, action, parameter_names);
+    uploadTableRowsFrom(row + 1, action, parameter_names, failures);
   } else {
     $.ajax(g_action_url,
            {type: 'POST',
@@ -268,13 +392,14 @@ function uploadTableRowsFrom(row, action, parameter_names) {
               if (ok && ok.length > 0) {
                 $('[data-row="' + row + '"] th').append('<span class="ok_outcome">OK</span>');
               } else {
+                ++failures;
                 $('[data-row="' + row + '"] th').append('<span class="failed_outcome">FAILED </span>');
                 var fail = xmldoc.documentElement.getElementsByTagName("failure");
                 if (fail && fail.length > 0) {
                   $('[data-row="' + row + '"] th').append(fail[0].childNodes[0].nodeValue);
                 }
               }
-              uploadTableRowsFrom(row + 1, action, parameter_names);
+              uploadTableRowsFrom(row + 1, action, parameter_names, failures);
             },
             error: function(jqXHR, textStatus, errorThrown) {
               alert("Error: " + textStatus + " / " + errorThrown);
@@ -286,26 +411,33 @@ function uploadTableRowsFrom(row, action, parameter_names) {
 function uploadTableRowsWithParameters(action, parameter_names) {
     // First row of file is labeled data-row="1".
     // If the file contains a header row, the first data to process is row 2, otherwise row 1.
-  uploadTableRowsFrom(header_row_present() ? 2 : 1, action, parameter_names);
+  uploadTableRowsFrom(header_row_present() ? 2 : 1, action, parameter_names, 0);
 }  
 
 function uploadTableRows(action) {
   uploadTableRowsWithParameters(action, collectDraggedLabels());
 }
 
-function onUploadComplete() {
+function onUploadComplete(failures) {
   $(".fields").addClass('hidden');
   $('#import_button_div').addClass('hidden');
 
-  $("#assign_label_message").text("Import successful!")
+  $("#assign_label_message")
+    .empty()
     .removeClass('hidden')
-    .css('color', '#008000');
+    .append("<span class='upload_complete_message'>Import complete!</span>");
+  if (failures > 0) {
+    $("#assign_label_message")
+      .append("<span class='upload_failure_message'> (" + failures + " failed)</span>");
+  }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // Document ready
 /////////////////////////////////////////////////////////////////////////////////////////
 $(function() {
+  buildEncodingSelector();
+
   // Establishes a drop target for putting a label back after being incorrectly
   // placed on a column.  
   $('.fields .target').droppable({
