1501f10b7b42674bdfc97e6539531f983f6be118
Jeff Piazza <jeff@jeffpiazza.org>
2018-12-15 09:01:23 -0500

    For slideshow kiosk, pre-fetch both images for the next racer while displaying current racer.  Noticeable performance improvement!

--- website/js/slideshow.js
+++ website/js/slideshow.js
@@ -1,8 +1,12 @@
-// We don't know the actual rendered size of the main photo until after it's loaded, so that's
-// when we can calculate how much margin to apply to the main photo.
+// We don't know the actual rendered size of the main photo until after it's
+// loaded, so that's when we can calculate how much margin to apply to the main
+// photo so it's vertically centered.  (If the image is taller than its
+// container, margin can be zero and the image will be scaled down to fit.)
 function mainphoto_onload(img) {
-  var top_margin = ($("#photo-background").height() - img.height) / 2;
-  $("#photo-background .mainphoto").css('margin-top', top_margin);
+  if (img.height < $("#photo-background").height()) {
+    var top_margin = ($("#photo-background").height() - img.height) / 2;
+    $(img).css('margin-top', top_margin);
+  }
 }
 
 (function() {
@@ -17,31 +21,48 @@ function mainphoto_onload(img) {
   };
 
   function refresh_page(racer) {
-    var div = $("#photo-background");
-    div.empty();
+    // There's always a div.next, which is hidden; we populate it with images
+    // for the next racer.  When the next racer becomes current, the old
+    // div.current gets removed, div.next becomes div.current, and we create a
+    // new div.next.
+    $("#photo-background div.current").remove();
     if (racer) {
-      div.append('<img class="mainphoto" onload="mainphoto_onload(this)" src="' +
-                 racer.getAttribute('main_photo') + '"/>');
-      div.append('<p class="subtitle">' + 
-                 '<span class="carno">' + racer.getAttribute('carnumber') + '</span>: ' +
-                 racer.getAttribute('name') +
-                 (racer.getAttribute('carname') ?
-                  '<br/><i>' + racer.getAttribute('carname') + '</i>' : '') +
-                 '</p>');
-      if (racer.hasAttribute('inset_photo')) {
-        div.append('<img class="inset_photo" src="' +
-                   racer.getAttribute('inset_photo') + '"/>');
+      var current = $("#photo-background div.next");
+      current.removeClass("next").addClass("current");
+      var next = $("<div class='next'></div>").appendTo("#photo-background");
+
+      if (current.find("img.mainphoto").length == 0) {
+        current.append('<img class="mainphoto" onload="mainphoto_onload(this)" src="' +
+                       racer.getAttribute('main_photo') + '"/>');
       }
+      current.find("img.mainphoto").after('<p class="subtitle">' + 
+                     '<span class="carno">' + racer.getAttribute('carnumber') + '</span>: ' +
+                     racer.getAttribute('name') +
+                     (racer.getAttribute('carname') ?
+                      '<br/><i>' + racer.getAttribute('carname') + '</i>' : '') +
+                     '</p>');
+      if (racer.hasAttribute('inset_photo') &&
+          current.find("img.inset_photo").length == 0) {
+        current.append('<img class="inset_photo" src="' +
+                       racer.getAttribute('inset_photo') + '"/>');
+      }
+
       // Preload the next image for better display
       if (racer.hasAttribute('next_photo')) {
-        div.append('<img style="display: none;" src="' +
+        next.append('<img class="mainphoto" onload="mainphoto_onload(this)" src="' +
                    racer.getAttribute('next_photo') + '"/>');
       }
+      if (racer.hasAttribute('next_inset')) {
+        next.append('<img class="inset_photo" src="' +
+                   racer.getAttribute('next_inset') + '"/>');
+      }
     } else {
+      // We assume there's no img.mainphoto under current, because there
+      // shouldn't have been any 'next' racer last time.
       var img = "photo.php/info/slideshow-title/" + cachebreaker + "/img/derby_car.png";
-      div.append('<img class="mainphoto" onload="mainphoto_onload(this)" src="' + img + '"/>');
+      current.append('<img class="mainphoto" onload="mainphoto_onload(this)" src="' + img + '"/>');
       if (kiosk_parameters.title) {
-        $('<p class="maintitle"></p>').text(kiosk_parameters.title).appendTo(div);
+        $('<p class="maintitle"></p>').text(kiosk_parameters.title).appendTo(current);
       }
     }
   }
