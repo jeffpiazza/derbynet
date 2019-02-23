b631002f9c8904c8d5a7cfe68ace5aa2f3e74abf
Jeff Piazza <jeff@jeffpiazza.org>
2019-02-23 10:13:09 -0500

    Add page for importing a previously-exported database snapshot, primarily for debugging.

---
+++ website/import-snapshot.php
@@ -0,0 +1,52 @@
+<?php @session_start();
+
+require_once('inc/banner.inc');
+require_once('inc/authorize.inc');
+require_permission(SET_UP_PERMISSION);
+
+?><!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title>Import Snapshot</title>
+<?php require('inc/stylesheet.inc'); ?>
+<link rel="stylesheet" type="text/css" href="css/import-snapshot.css"/>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/import-snapshot.js"></script>
+</head>
+<body>
+<?php make_banner('Import Database Snapshot', 'setup.php'); ?>
+
+<div class="warning">
+    <span class="red">WARNING:</span> Importing a database snapshot<br/>
+    will <b>delete</b> all the data<br/>
+    that's currently in the database.
+</div>
+
+<form id="upload-form">
+  <input type="hidden" name="action" value="snapshot.put"/>
+  <div class="file_target">
+    <input type="file" id="snapshot_file" name="snapshot" data-enhanced="true"/>
+    <label for="snapshot_file">
+      <div id="drop_img"><img src="img/drop.png"/></div>
+      <div id="please_select_message">
+         Please select a previously-exported snapshot to import<br/>or drag it here.
+      </div>
+    </label>
+    <div id="filepath"></div>
+  </div>
+
+<p>&nbsp;</p>
+
+<div class="block_buttons">
+  <input type="submit" id="submit-snapshot"/>
+</div>
+
+</form>
+
+<?php
+  require_once('inc/ajax-pending.inc');
+  require_once('inc/ajax-failure.inc');
+?>
+</body>
+</html>
---
+++ website/js/import-snapshot.js
@@ -0,0 +1,46 @@
+function onFormSubmit(e) {
+  e.preventDefault();
+  $.ajax({
+    url: "action.php",
+    type: "POST",
+    data:  new FormData(this),
+   contentType: false,
+         cache: false,
+   processData:false,
+    success: function(data) {
+      var succ = xmldoc.documentElement.getElementsByTagName("success");
+      if (succ && succ.length > 0) {
+        window.location = "setup.php";
+      }
+   },
+  });
+}
+
+function enableOrDisableSubmitButton() {
+  var hasFile = $("#snapshot_file").val() != '';
+  $("#submit-snapshot").prop('disabled', !hasFile);
+  $("#upload-form label").toggleClass('hidden', hasFile);
+  $(".file_target").toggleClass('draghover', hasFile);
+  $("#filepath").toggleClass('hidden', !hasFile);
+  if (hasFile) {
+    $("#filepath").text($("#snapshot_file")[0].files[0].name);
+  }
+}
+
+$(function() {
+
+  $(".file_target input").on('dragenter', function() {
+    // Event is received by the <input> element, whose parent is the
+    // .file_target div
+    $(event.target).parent().addClass("draghover");
+  });
+  $(".file_target input").on('dragleave', function() {
+    $(event.target).parent().removeClass("draghover");
+  });
+  $(".file_target input").on('change', enableOrDisableSubmitButton);
+
+  enableOrDisableSubmitButton();
+
+  $("#upload-form").on("submit", onFormSubmit);
+});
+
