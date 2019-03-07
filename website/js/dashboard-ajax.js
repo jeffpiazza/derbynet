ecc2cd3f476f3c8d78f3a74e553917c3adb2d2c9
Jeff Piazza <jeff@jeffpiazza.org>
2019-03-07 07:05:49 -0500

    Add flag to try to prevent ajax errors being reported when leaving a page with an outstanding ajax request.

--- website/js/dashboard-ajax.js
+++ website/js/dashboard-ajax.js
@@ -3,6 +3,12 @@
 
 g_action_url = "action.php";
 
+// This variable gets set to true when leaving a page; it should prevent errors from
+// showing for any in-flight ajax calls.
+var g_unloading = false;
+
+$(window).bind("beforeunload", function() { g_unloading = true; });
+
 // Note that this doesn't run if the $.ajax call has a 'success:' callback that
 // generates an error.
 $(document).ajaxSuccess(function(event, xhr, options, xmldoc) {
@@ -23,13 +29,15 @@ $(document).ajaxSuccess(function(event, xhr, options, xmldoc) {
 });
 
 $(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
-  console.log("ajaxError: " + thrownError);
-  console.log(thrownError);
-  console.log(jqXHR);
-  console.log("Response text: " + jqXHR.responseText);
-  console.log("ReadyState: " + jqXHR.readyState);
-  console.log("status: " + jqXHR.status);
-  console.log(event);
+  if (!g_unloading) {
+    console.log("ajaxError: " + thrownError);
+    console.log(thrownError);
+    console.log(jqXHR);
+    console.log("Response text: " + jqXHR.responseText);
+    console.log("ReadyState: " + jqXHR.readyState);
+    console.log("status: " + jqXHR.status);
+    console.log(event);
 	alert("Ajax error: " + thrownError);
+  }
 });
 
