64e3b32c2fbf91c0d5acd60cdcac43135ff08a50
Jeff Piazza <jeff@jeffpiazza.org>
2019-04-14 17:01:12 -0400

    Update installers to allow 16M php uploads

--- installer/mac/scripts/webserver/create-or-update-php-ini.sh
+++ installer/mac/scripts/webserver/create-or-update-php-ini.sh
@@ -16,15 +16,18 @@ TIMEZONE=`systemsetup -gettimezone | sed -e "s/Time Zone: //"`
 
 # How large are the pictures you want to upload?  8M covers an 18-megapixel
 # photo.
+MAX_UPLOAD_SIZE=16M
+
 #
 # memory_limit = 128M is insufficient for photo_crop to rotate an 8M image; 256M
-# is.
+# is.  Not sure what's required for a full 16M image to rotate.
 #
 # The default session.gc_maxlifetime value can cause a session (including
 # logged-in state of the user) to expire after about 24 minutes of inactivity --
 # inconvenient for humans, and even more serious for headless scripts that don't
 # re-login.
-sed -e "s#^upload_max_filesize = 2M#upload_max_filesize = 8M#" \
+sed -e "s#^upload_max_filesize = [28]M#upload_max_filesize = $MAX_UPLOAD_SIZE#" \
+    -e "s#^post_max_size = [2-8]M#post_max_size = $MAX_UPLOAD_SIZE#" \
     -e "s#^;date.timezone =#date.timezone = $TIMEZONE#" \
     -e "s#^memory_limit = 128M#memory_limit = 256M#" \
     -e "s#^;error_log = syslog#error_log = /var/log/php_errors#" \
