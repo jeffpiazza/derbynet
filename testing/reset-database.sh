69eb3a50f8b8ee46d9d97165e0fd96aab1721ae1
Jeff Piazza <jeff@jeffpiazza.org>
2019-08-22 07:19:07 -0400

    Remove test scripts from testing/setup-basic.sh, so it actually sets up a sample database.  Also ensure we're using a test database and not clobbering real data.
    In reset-database.sh, preserve video directory settings just as we now preserve photo directory settings.

--- testing/reset-database.sh
+++ testing/reset-database.sh
@@ -11,6 +11,8 @@ PHOTO_DIR=$(curl_get "action.php?query=settings" | xmllint --format - | sed -ne 
 
 CAR_PHOTO_DIR=$(curl_get "action.php?query=settings" | xmllint --format - | sed -ne '/key="car-photo-directory"/ s#[^>]*>\([^<]*\)</setting>#\1#p')
 
+VIDEO_DIR=$(curl_get "action.php?query=settings" | xmllint --format - | sed -ne '/key="video-directory"/ s#[^>]*>\([^<]*\)</setting>#\1#p')
+
 curl_post action.php "action=database.execute&script=schema" | check_success
 curl_post action.php "action=database.execute&script=update-schema" | check_success
 
@@ -19,5 +21,8 @@ if [ "$PHOTO_DIR" ] ; then
 fi
 if [ "$CAR_PHOTO_DIR" ] ; then
     curl_post action.php "action=settings.write&car-photo-dir=$CAR_PHOTO_DIR" | check_success
+fi
+if [ "$VIDEO_DIR" ] ; then
+    curl_post action.php "action=settings.write&video-dir=$VIDEO_DIR" | check_success
 fi
 
--- testing/setup-basic.sh
+++ testing/setup-basic.sh
@@ -8,6 +8,16 @@ shift
 set -e -E -o pipefail
 source `dirname $0`/common.sh
 
+# Force to a test sqlite database -- don't accidentally clobber the current database!
+DBPATH=${1:-/Library/WebServer/Documents/xsite/local/trial.sqlite}
+
+user_login_coordinator
+
+curl_post action.php \
+        "action=setup.nodata&connection_string=sqlite:$DBPATH&dbuser=&dbpass=" \
+        | check_success
+
+
 `dirname $0`/reset-database.sh "$BASE_URL"
 `dirname $0`/import-roster.sh "$BASE_URL"
 `dirname $0`/test-den-changes.sh "$BASE_URL"
@@ -15,7 +25,5 @@ source `dirname $0`/common.sh
 `dirname $0`/photo-setup.sh "$BASE_URL"
 `dirname $0`/test-photo-upload.sh "$BASE_URL"
 
-`dirname $0`/test-basic-racing.sh "$BASE_URL"
-
-`dirname $0`/test-awards.sh "$BASE_URL" basic
+# `dirname $0`/test-awards.sh "$BASE_URL" basic
 `dirname $0`/test-photo-assignments.sh "$BASE_URL"
