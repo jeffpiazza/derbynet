e0f384152ed32e9af8b99f276ea93331d2d74c9f
Andrew Speck <15779151+ajspeck@users.noreply.github.com>
2019-12-28 20:24:37 -0500

    update webcamJS + improve camera switching for ios devices

--- website/js/checkin.js
+++ website/js/checkin.js
@@ -307,7 +307,7 @@ function arm_webcam_dialog() {
     if (!loaded) {
       disable_preview('You may have to enable Flash, or give permission to use your webcam.');
     }
-  }, 2000);
+  }, 5000);
 }
 
 // For #photo_drop form:
@@ -338,6 +338,17 @@ Dropzone.options.photoDrop = {
 
 
 function switch_camera_modal() {
+  var i = 0;
+
+  navigator.mediaDevices.enumerateDevices()
+  .then(function(devices) {
+    devices.forEach(function(device) {
+      if( device.kind=== "videoinput") {
+        g_cameras[i]= device.deviceId;
+        i++;
+      }
+    });
+  });
   g_cameraIndex++;
   if ( g_cameraIndex >= g_cameras.length ) {
     g_cameraIndex = 0;
@@ -352,7 +363,7 @@ function switch_camera_modal() {
 	  crop_width: g_width,
 	  crop_height: g_height,
 	  constraints: {
-		  deviceId: g_cameras[g_cameraIndex]
+		  deviceId: {exact: g_cameras[g_cameraIndex]}
 	  }
   });
   Webcam.attach('#preview');
