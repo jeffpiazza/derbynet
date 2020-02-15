a80efbce166db1ec6999e49822f5a5034682a83f
heatvent <matteocp@gmail.com>
2020-02-15 13:35:48 -0600

    Change arguments passed to derby-timer.sh for more control (#121)
    * Create document.inc
    
    * Update document.inc
    
    Initial editing of document
    
    * Update derby-timer.sh
    
    Modified so you can pass different arguments rather than just username and password.  This is useful to run headless or to run int simulate timer mode.
    
    * Update derbynet.conf
    
    Added TIMER_ARGS to the derbynet.conf file as well as usage from the derby-timer help.
    
    * Delete document.inc
    
    * Update derbynet.conf
    
    Updated based on Jeff's suggestion
    
    * Update derbynet.conf
    
    Jeff, is this what you are looking for?  
    
    I guess I was thinking TIMER_ARGS could just be passed, not sure what the benefit is to incrementally constructing vs. just typing in the arguments that need to get passed.  In the example, couldn't someone just put in TIMER_ARGS="-u Timer -p ".
    
    For line 38, I copy/pasted what you had in the response.  Should there be a -p before TIMER_PASSWORD and no equals sign?
    
    Sorry if I am making this more complicated or missing the boat ... I am not a coder by trade or by hobby :)
    
    * Update derbynet.conf
    
    Updated TIMER_ARGS to construct arguments incrementally.

--- extras/scripts/derby-timer.sh
+++ extras/scripts/derby-timer.sh
@@ -3,4 +3,4 @@
 test -f /etc/derbynet.conf  && . /etc/derbynet.conf
 test -f /boot/derbynet.conf && . /boot/derbynet.conf
 
-java -jar /usr/bin/derby-timer.jar -u $TIMER_USER -p $TIMER_PASSWORD $DERBYNET_SERVER
+java -jar /usr/bin/derby-timer.jar $TIMER_ARGS $DERBYNET_SERVER
