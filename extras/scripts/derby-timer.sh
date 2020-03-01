d9c569a4d0f38e63df697bec566ff8e0f7207363
Jeff Piazza <jeff@jeffpiazza.org>
2020-03-01 12:23:31 -0500

    derby-timer.sh: Use curl to preflight that the server is reachable before attempting to start derby-timer.jar.

--- extras/scripts/derby-timer.sh
+++ extras/scripts/derby-timer.sh
@@ -1,6 +1,18 @@
 #! /bin/sh
 
+TIMER_ARGS=
+
 test -f /etc/derbynet.conf  && . /etc/derbynet.conf
 test -f /boot/derbynet.conf && . /boot/derbynet.conf
+
+# Wait for server and network to come up
+CONTACT_OK=0
+while [ $CONTACT_OK -eq 0 ]; do
+    echo Attempting to contact $DERBYNET_SERVER
+    curl --insecure --connect-timeout 6 --location --silent \
+         "$DERBYNET_SERVER/index.php" > /dev/null && \
+        CONTACT_OK=1
+done
+echo Starting timer jar
 
 java -jar /usr/bin/derby-timer.jar $TIMER_ARGS $DERBYNET_SERVER
