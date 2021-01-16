8ae3a3776892c3411cf69a83ab3ab7899322c692
Jeff Piazza <jeff@jeffpiazza.org>
2021-01-15 20:24:00 -0500

    Bug fix in test-racer-query.sh, to account for rank order within the class.

--- testing/test-racer-query.sh
+++ testing/test-racer-query.sh
@@ -8,6 +8,6 @@ curl_get "action.php?query=racer.list" | grep -e '<racer ' | head -n 1 | expect_
 
 curl_get "action.php?query=racer.list&order=name" | grep '<racer ' | head -n 1 | expect_one Akiyama || true
 curl_get "action.php?query=racer.list&order=class" | grep '<racer ' | head -n 1 | expect_one Asher || true
-curl_get "action.php?query=racer.list&order=rank" | grep '<racer ' | head -n 1 | expect_one Asher || true
+curl_get "action.php?query=racer.list&order=rank" | grep '<racer ' | head -n 1 | expect_one Bittinger || true
 curl_get "action.php?query=racer.list&order=car" | grep '<racer ' | head -n 1 | expect_one Asher || true
 curl_get "action.php?query=racer.list&order=checkin" | grep '<racer ' | head -n 1 | expect_one Fizer || true
