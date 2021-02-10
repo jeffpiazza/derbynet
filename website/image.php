a0f539d439996737b986e12f3d3709fe7b9df1d1
Jeff Piazza <jeff@jeffpiazza.org>
2021-02-10 16:53:42 -0500

    Shorten caching period for image.php content, so that changes to the image set take effect within two minutes.

--- website/image.php
+++ website/image.php
@@ -13,23 +13,26 @@ require_once('inc/photo-config.inc');
 $exploded = explode('/', path_info());
 if (count($exploded) == 2) {
   $file_path = image_file_path($exploded[1]);
-
-  if (is_readable($file_path)) {
-    header('Pragma: public');
-    header('Cache-Control: max-age=86400, public');
-    header('Expires: '.gmdate('D, d M Y H:i:s', time() + 86400).' GMT');
-    header('Content-type: '.pseudo_mime_content_type($file_path));
-
-    readfile($file_path);
-  } else {
-    header('File-path: '.$file_path);
-    header('file_exists: '.file_exists($file_path));
-    header('file: '.filesize($file_path));
-
-    echo "Can't read file.";
-  }
 } else {
   exit(1);
+}
+
+
+if (is_readable($file_path)) {
+  // Cache renewal required every 2 minutes (120 seconds), to get most of the
+  // benefit of caching while allowing changing image sets in a "reasonable"
+  // amount of time.
+  header('Cache-Control: max-age=120, public');
+  header('Expires: '.gmdate('D, d M Y H:i:s', time() + 120).' GMT');
+  header('Content-type: '.pseudo_mime_content_type($file_path));
+
+  readfile($file_path);
+} else {
+  header('File-path: '.$file_path);
+  header('file_exists: '.file_exists($file_path));
+  header('file: '.filesize($file_path));
+
+  echo "Can't read file.";
 }
 
 ?>
