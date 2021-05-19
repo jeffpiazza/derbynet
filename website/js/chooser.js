4dbf93f9408e78db11aa33b9b0f19ccf38cceea7
Jeff Piazza <jeff@jeffpiazza.org>
2021-05-19 08:34:25 -0400

    Convert file actions and queries to json.
    Former-commit-id: 1c0b184d1ad39c633579ea7b50ebda0b4e384771

--- website/js/chooser.js
+++ website/js/chooser.js
@@ -16,73 +16,64 @@ function choosing_output_file() { return g_file_chooser_mode == 'file.out'; }
 function choosing_file() { return choosing_output_file(); }
 
 function repopulate_chooser_modal(path) {
-  console.log("repopulate_chooser_modal with " + path);  // TODO
   $("#chooser_directories_above").empty();
   $("#chooser_directory_content").empty();
   $.ajax('action.php',
          {type: 'GET',
-          data: {query: 'file.directory.nodata',
+          data: {query: 'json.file.directory.nodata',
                  path: path},
           // Disable the alert from dashboard-ajax.js that would show up for
           // "unable to list directory"
           global: false,
           success: function(data) {
-            var root = data.getElementsByTagName("directory");
-            if (root.length == 0) {
+            if (!data.hasOwnProperty('path')) {
               return;
             }
-            root = root[0];
-            var path = root.getElementsByTagName("path");
-            if (path.length == 0) {
-              return;
-            }
-            path = path[0].getAttribute("realpath"); // Includes trailing directory separator
+            var path = data.path;
             $("#chooser_directory_path").val(path);
 
-            var chosen = root.getElementsByTagName("chosen");
-            if (chosen.length > 0) {
-              $("#chooser_file_name").val(chosen[0].textContent);
+            if (data.hasOwnProperty('chosen')) {
+              $("#chooser_file_name").val(data.chosen);
             }
 
-            var supers = root.getElementsByTagName("base");
+            var supers = data['base-paths'];
             for (var i = 0; i < supers.length; ++i) {
-              var text = supers[i].textContent;
+              var text = supers[i].basename;
               if (text.length == 0) {
                 text = "/";  // TODO Does Windows encounter this case?
               }
               $("<option/>").prependTo($("#chooser_directories_above"))
-                .text(text).prop('value', supers[i].getAttribute('path'));
+                .text(text).prop('value', supers[i].path);
             }
 
             var last = $("#chooser_directories_above option").filter(":last");
             last.attr("selected", "selected");
             mobile_select_refresh($("#chooser_directories_above"));
 
-            // <file readable writable directory> xyz </file>
-            var files = data.getElementsByTagName("file");
+            var files = data.files;
             for (var i = 0; i < files.length; ++i) {
               var li = $("<li></li>").appendTo($("#chooser_directory_content"));
-              if (files[i].getAttribute("directory") == "1") {
-                if (files[i].getAttribute("readable") == "1") {
-                  li.text(files[i].textContent)
+              if (files[i].isdir) {
+                if (files[i].readable) {
+                  li.text(files[i].file)
                     .addClass('icon-right button')
                     .on("click", null, path, chooser_visit_subdirectory);
-                  if (files[i].getAttribute("writable") == "0") {
+                  if (files[i].writable) {
                     li.addClass('unwritable');
                   } else {
                     li.addClass('writable');
                   }
                 } else {
                   // If it's not readable, then we can't explore, but show the icon-right icon anyway.
-                  li.text(files[i].textContent)
+                  li.text(files[i].file)
                     .addClass("icon-right")
                     .addClass("unreadable");
                 }
               } else {
                 if (choosing_directory()) {
-                  li.text(files[i].textContent).addClass("ui-li-static");
+                  li.text(files[i].file).addClass("ui-li-static");
                 } else {
-                  li.text(files[i].textContent)
+                  li.text(files[i].file)
                     .addClass("button")
                     .on("click", null, path, chooser_select_existing);
                 }
@@ -113,22 +104,21 @@ function chooser_select_existing(event) {
 }
 
 function chooser_show_add_directory_modal(event) {
-    var parent_path = event.data;
-    show_tertiary_modal("#chooser_add_directory_modal", function() {
-        chooser_close_add_directory_modal();
-        $.ajax('action.php',
-               {type: 'POST',
-                data: {action: 'file.directory.new.nodata',
-                       directory: parent_path + $("#chooser_new_directory_name").val()},
-                success: function(data) {
-                    var path = data.getElementsByTagName("path");
-                    if (path.length > 0) {
-                        repopulate_chooser_modal(path[0].textContent);
-                    }
-                }
-               });
-        return false;
-    });
+  var parent_path = event.data;
+  show_tertiary_modal("#chooser_add_directory_modal", function() {
+    chooser_close_add_directory_modal();
+    $.ajax('action.php',
+           {type: 'POST',
+            data: {action: 'json.file.directory.new.nodata',
+                   directory: parent_path + $("#chooser_new_directory_name").val()},
+            success: function(data) {
+              if (data.hasOwnProperty('path')) {
+                repopulate_chooser_modal(data.path);
+              }
+            }
+           });
+    return false;
+  });
 }
 
 function chooser_close_add_directory_modal() {
--- website/js/settings.js
+++ website/js/settings.js
@@ -44,33 +44,32 @@ function on_max_runs_change() {
 }
 
 function render_directory_status_icon(photo_dir_selector) {
-    $.ajax('action.php',
-           {type: 'GET',
-            data: {query: 'file.stat',
-                   path: $(photo_dir_selector).val()},
-            success: function(data) {
-                var icon_span = $(photo_dir_selector + '_icon');
-                var msg_para = $(photo_dir_selector + '_message');
-                var path = data.getElementsByTagName("path");
-                if (path.length > 0) {
-                    path = path[0];
-                    if (path.getAttribute("directory") == "0" ||
-                        path.getAttribute("readable") == "0") {
-                        icon_span.html('<img src="img/status/trouble.png"/>');
-                        msg_para.text('Directory does not exist or is not readable.');
-                    } else if (path.getAttribute("writable") == "0") {
-                        icon_span.html('<img src="img/status/readonly.png"/>');
-                        msg_para.text('Directory is not writable.');
-                    } else {
-                        icon_span.html('<img src="img/status/ok.png"/>');
-                        msg_para.text('');
-                    }
-                } else {
-                    icon_span.html("");
-                    msg_para.text('');
-                }
+  $.ajax('action.php',
+         {type: 'GET',
+          data: {query: 'json.file.stat',
+                 path: $(photo_dir_selector).val()},
+          success: function(data) {
+            console.log(data);
+            var icon_span = $(photo_dir_selector + '_icon');
+            var msg_para = $(photo_dir_selector + '_message');
+            if (data.hasOwnProperty('stat')) {
+              var stat = data.stat;
+              if (!stat.isdir || !stat.readable) {
+                icon_span.html('<img src="img/status/trouble.png"/>');
+                msg_para.text('Directory does not exist or is not readable.');
+              } else if (!stat.writable) {
+                icon_span.html('<img src="img/status/readonly.png"/>');
+                msg_para.text('Directory is not writable.');
+              } else {
+                icon_span.html('<img src="img/status/ok.png"/>');
+                msg_para.text('');
+              }
+            } else {
+              icon_span.html("");
+              msg_para.text('');
             }
-           });
+          }
+         });
 }
 
 function browse_for_photo_directory(photo_dir_selector) {
