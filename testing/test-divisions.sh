e4c222c4bbc8b7e8be888acbd8d226a92ba12fbb
Jeff Piazza <jeff@jeffpiazza.org>
2021-08-07 13:35:10 -0400

    Reorder divisions and subdivisions when classes and ranks are re-ordered.

--- testing/test-divisions.sh
+++ testing/test-divisions.sh
@@ -21,14 +21,64 @@ curl_getj "action.php?query=poll&values=divisions" | \
 
 curl_getj "action.php?query=poll&values=classes" | \
     jq -e '.classes | length == 3 
-        and .[0].name == "Default" and (.[0].subgroups | length == 2)
+        and .[0].name == "Default" and .[0].classid == 1
+        and (.[0].subgroups | length == 2)
         and .[0].subgroups[0].name == "Subdiv 1a"
+        and .[0].subgroups[0].rankid == 1
         and .[0].subgroups[1].name == "Subdiv 1b"
-        and .[1].name == "Div 2" and (.[1].subgroups | length == 2)
+        and .[0].subgroups[1].rankid == 2
+        and .[1].name == "Div 2" and .[1].classid == 2
+        and (.[1].subgroups | length == 2)
         and .[1].subgroups[0].name == "Subdiv 2b"
+        and .[1].subgroups[0].rankid == 3
         and .[1].subgroups[1].name == "Subdiv 2a"
-        and .[2].name == "Div 3" and (.[2].subgroups | length == 1)
+        and .[1].subgroups[1].rankid == 4
+        and .[2].name == "Div 3" and .[2].classid == 3
+        and (.[2].subgroups | length == 1)
         and .[2].subgroups[0].name == "Div 3"
+        and .[2].subgroups[0].rankid == 5
+' >/dev/null || test_fails
+
+# With by-division rule, reordering classes should reorder divisions
+curl_postj action.php "action=class.order&classid_1=2&classid_2=1&classid_3=3" | check_jsuccess
+curl_getj "action.php?query=poll&values=classes" | \
+    jq -e '.classes | length == 3 
+        and .[0].name == "Div 2" and .[0].classid == 2
+        and (.[0].subgroups | length == 2)
+        and .[0].subgroups[0].name == "Subdiv 2b"
+        and .[0].subgroups[0].rankid == 3
+        and .[0].subgroups[1].name == "Subdiv 2a"
+        and .[0].subgroups[1].rankid == 4
+        and .[1].name == "Default" and .[1].classid == 1
+        and (.[1].subgroups | length == 2)
+        and .[1].subgroups[0].name == "Subdiv 1a"
+        and .[1].subgroups[0].rankid == 1
+        and .[1].subgroups[1].name == "Subdiv 1b"
+        and .[1].subgroups[1].rankid == 2
+        and .[2].name == "Div 3" and .[2].classid == 3
+        and (.[2].subgroups | length == 1)
+        and .[2].subgroups[0].name == "Div 3"
+        and .[2].subgroups[0].rankid == 5
+' >/dev/null || test_fails
+
+curl_getj "action.php?query=poll&values=divisions" | \
+    jq -e '.divisions | length == 3 
+        and .[0].name == "Div 2" and .[0].divisionid == 2
+        and (.[0].subdivisions | length == 2)
+        and .[0].subdivisions[0].name == "Subdiv 2b"
+        and .[0].subdivisions[0].subdivisionid == 3
+        and .[0].subdivisions[1].name == "Subdiv 2a"
+        and .[0].subdivisions[1].subdivisionid == 4
+        and .[1].name == "Default" and .[1].divisionid == 1
+        and (.[1].subdivisions | length == 2)
+        and .[1].subdivisions[0].name == "Subdiv 1a"
+        and .[1].subdivisions[0].subdivisionid == 1
+        and .[1].subdivisions[1].name == "Subdiv 1b"
+        and .[1].subdivisions[1].subdivisionid == 2
+        and .[2].name == "Div 3" and .[2].divisionid == 3
+        and (.[2].subdivisions | length == 1)
+        and .[2].subdivisions[0].name == "Div 3"
+        and .[2].subdivisions[0].subdivisionid == 5
 ' >/dev/null || test_fails
 
 curl_postj action.php "action=division.apply-rule&rule=one-group" | check_jsuccess
@@ -36,10 +86,46 @@ curl_postj action.php "action=division.apply-rule&rule=one-group" | check_jsucce
 curl_getj "action.php?query=poll&values=classes" | \
     jq -e '.classes | length == 1
         and .[0].name == "One Group" and (.[0].subgroups | length == 3)
-        and .[0].subgroups[0].name == "Default"
-        and .[0].subgroups[1].name == "Div 2"
+        and .[0].subgroups[0].name == "Div 2"
+        and .[0].subgroups[0].rankid == 2
+        and .[0].subgroups[1].name == "Default"
+        and .[0].subgroups[1].rankid == 1
         and .[0].subgroups[2].name == "Div 3"
+        and .[0].subgroups[2].rankid == 3
 ' >/dev/null || test_fails
+
+# With one-group rule, reordering subdivisions should reorder divisions
+curl_postj action.php "action=rank.order&rankid_1=1&rankid_2=2&rankid_3=3" | check_jsuccess
+curl_getj "action.php?query=poll&values=classes" | \
+    jq -e '.classes | length == 1
+        and .[0].name == "One Group" and (.[0].subgroups | length == 3)
+        and .[0].subgroups[0].name == "Default"
+        and .[0].subgroups[0].rankid == 1
+        and .[0].subgroups[1].name == "Div 2"
+        and .[0].subgroups[1].rankid == 2
+        and .[0].subgroups[2].name == "Div 3"
+        and .[0].subgroups[2].rankid == 3
+' >/dev/null || test_fails
+curl_getj "action.php?query=poll&values=divisions" | \
+    jq -e '.divisions | length == 3 
+        and .[0].name == "Default" and .[0].divisionid == 1
+        and (.[0].subdivisions | length == 2)
+        and .[0].subdivisions[0].name == "Subdiv 1a"
+        and .[0].subdivisions[0].subdivisionid == 1
+        and .[0].subdivisions[1].name == "Subdiv 1b"
+        and .[0].subdivisions[1].subdivisionid == 2
+        and .[1].name == "Div 2" and .[1].divisionid == 2
+        and (.[1].subdivisions | length == 2)
+        and .[1].subdivisions[0].name == "Subdiv 2b"
+        and .[1].subdivisions[0].subdivisionid == 3
+        and .[1].subdivisions[1].name == "Subdiv 2a"
+        and .[1].subdivisions[1].subdivisionid == 4
+        and .[2].name == "Div 3" and .[2].divisionid == 3
+        and (.[2].subdivisions | length == 1)
+        and .[2].subdivisions[0].name == "Div 3"
+        and .[2].subdivisions[0].subdivisionid == 5
+' >/dev/null || test_fails
+
 
 curl_postj action.php "action=division.apply-rule&rule=by-division" | check_jsuccess
 
@@ -84,6 +170,18 @@ curl_getj "action.php?query=poll&values=classes" | \
         and .[2].subgroups[0].name == "Div 3" and .[2].subgroups[0].rankid == 5
 ' >/dev/null || test_fails
 
+curl_getj "action.php?query=poll&values=divisions" | \
+    jq -e '.divisions | length == 3
+        and .[0].name == "Default" and (.[0].subdivisions | length == 2)
+        and .[0].subdivisions[0].name == "Subdiv 1a" and .[0].subdivisions[0].subdivisionid == 1
+        and .[0].subdivisions[1].name == "Subdiv 1b" and .[0].subdivisions[1].subdivisionid == 2
+        and .[1].name == "Div 2" and (.[1].subdivisions | length == 2)
+        and .[1].subdivisions[0].name == "Subdiv 2b" and .[1].subdivisions[0].subdivisionid == 3
+        and .[1].subdivisions[1].name == "Subdiv 2a" and .[1].subdivisions[1].subdivisionid == 4
+        and .[2].name == "Div 3" and (.[2].subdivisions | length == 1)
+        and .[2].subdivisions[0].name == "Div 3" and .[2].subdivisions[0].subdivisionid == 5
+' >/dev/null || test_fails
+
 curl_postj action.php "action=division.apply-rule&rule=custom&cleanup=1" | check_jsuccess
 
 curl_getj "action.php?query=poll&values=classes" | \
@@ -100,6 +198,7 @@ curl_getj "action.php?query=poll&values=classes" | \
 
 curl_postj action.php "action=division.move&div_field=subdivisionid&div_id=3&group_field=classid&group_id=-1" | check_jsuccess
 
+
 curl_getj "action.php?query=poll&values=classes" | \
     jq -e '.classes | length == 4
         and .[0].name == "Default" and (.[0].subgroups | length == 3)
@@ -112,5 +211,16 @@ curl_getj "action.php?query=poll&values=classes" | \
         and .[2].subgroups[0].name == "Div 3" and .[2].subgroups[0].rankid == 5
         and .[3].name == "Subdiv 2b" and (.[3].subgroups | length == 1)
         and .[3].subgroups[0].name == "Subdiv 2b" and .[3].subgroups[0].rankid == 7
+' >/dev/null || test_fails
+curl_getj "action.php?query=poll&values=divisions" | \
+    jq -e '.divisions | length == 3
+        and .[0].name == "Default" and (.[0].subdivisions | length == 2)
+        and .[0].subdivisions[0].name == "Subdiv 1a" and .[0].subdivisions[0].subdivisionid == 1
+        and .[0].subdivisions[1].name == "Subdiv 1b" and .[0].subdivisions[1].subdivisionid == 2
+        and .[1].name == "Div 2" and (.[1].subdivisions | length == 2)
+        and .[1].subdivisions[0].name == "Subdiv 2a" and .[1].subdivisions[0].subdivisionid == 4
+        and .[1].subdivisions[1].name == "Subdiv 2b" and .[1].subdivisions[1].subdivisionid == 3
+        and .[2].name == "Div 3" and (.[2].subdivisions | length == 1)
+        and .[2].subdivisions[0].name == "Div 3" and .[2].subdivisions[0].subdivisionid == 5
 ' >/dev/null || test_fails
 
