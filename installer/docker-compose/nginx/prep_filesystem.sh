bc563e62d7035eb6d46efd17ea9ea673b47fb66e
Scott Hiemstra <scott@hiemstra.us>
2022-11-29 20:23:23 -0500

    Adding comments

--- installer/docker-compose/nginx/prep_filesystem.sh
+++ installer/docker-compose/nginx/prep_filesystem.sh
@@ -7,11 +7,16 @@ then
     else
         echo "$WWW_ROOT is Empty, creating DerbyNet Website filesystem."
         cd /
-        git clone https://github.com/jeffpiazza/derbynet.git && \
-            mv /derbynet/website/* /usr/share/nginx/html/. && \
-            mv /derbynet/templates/banner.inc /usr/share/nginx/html/inc/banner.inc && \
-            rm -rf /derbynet && \
-            test -d $WWW_ROOT/local || mkdir -m 777 $WWW_ROOT/local
+        echo "Cloning Jeff's repo."
+        git clone https://github.com/jeffpiazza/derbynet.git
+        echo "Moving the website files into $WWW_ROOT."
+        mv /derbynet/website/* $WWW_ROOT/.
+        echo "Moving the missing banner.inc file into place."
+        mv /derbynet/templates/banner.inc $WWW_ROOT/inc/banner.inc
+        echo "Getting rid of the Derbynet repo to conserve space."
+        rm -rf /derbynet
+        echo "Fixing permissions on $WWW_ROOT/local"
+        test -d $WWW_ROOT/local || mkdir -m 777 $WWW_ROOT/local
     fi
 else
 	echo "Directory $DIR not found."
