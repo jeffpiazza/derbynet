991c29c2548bd5444a2b48884995f7b0b2c63b8d
Jeff Piazza <jeff@jeffpiazza.org>
2023-04-16 19:54:40 -0400

    Update setup-basic.sh and setup-basic-no-photo.sh scripts to set up testdb instead of trying to point to a local file.

--- testing/setup-basic-no-photos.sh
+++ testing/setup-basic-no-photos.sh
@@ -2,20 +2,17 @@
 
 BASE_URL=$1
 shift
-DBTYPE=$1
-shift
+if [ -z "$BASE_URL" ] ; then
+  BASE_URL=localhost
+fi
+
 
 set -e -E -o pipefail
 source `dirname $0`/common.sh
 
-# Force to a test sqlite database -- don't accidentally clobber the current database!
-DBPATH=${1:-/Library/WebServer/Documents/xsite/local/trial.sqlite}
-
 user_login_coordinator
 
-curl_postj action.php \
-        "action=setup.nodata&connection_string=sqlite:$DBPATH&dbuser=&dbpass=" \
-        | check_jsuccess
+curl_postj action.php "action=setup.nodata&ez-new=testdb" > /dev/null
 
 `dirname $0`/reset-database.sh "$BASE_URL"
 `dirname $0`/import-roster.sh "$BASE_URL"
--- testing/setup-basic.sh
+++ testing/setup-basic.sh
@@ -2,14 +2,13 @@
 
 BASE_URL=$1
 shift
-DBTYPE=$1
-shift
+if [ -z "$BASE_URL" ] ; then
+  BASE_URL=localhost
+fi
+
 
 set -e -E -o pipefail
 source `dirname $0`/common.sh
-
-# Force to a test sqlite database -- don't accidentally clobber the current database!
-DBPATH=${1:-/Library/WebServer/Documents/xsite/local/trial.sqlite}
 
 `dirname $0`/setup-basic-no-photos.sh "$BASE_URL"
 
