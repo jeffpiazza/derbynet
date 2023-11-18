fbf82610a5272ef34f8d1763791f8e4969b04975
Jeff Piazza <jeff@jeffpiazza.org>
2023-11-17 19:05:17 -0500

    Search for imagery and slides in the umbrella data directory, as well as in event-specific directories.

--- installer/mac/webserver/scripts/maybe-create-data-directories.sh
+++ installer/mac/webserver/scripts/maybe-create-data-directories.sh
@@ -5,6 +5,12 @@ mkdir -m 777 /Library/WebServer/Documents/derbynet/local
 mkdir -m 775 "$HOME/Public/DerbyNet"
 chown $USER:_www "$HOME/Public/DerbyNet"
 
+mkdir -m 775 "$HOME/Public/DerbyNet/imagery"
+chown $USER:_www "$HOME/Public/DerbyNet/imagery"
+
+mkdir -m 775 "$HOME/Public/DerbyNet/slides"
+chown $USER:_www "$HOME/Public/DerbyNet/slides"
+
 # V2.0 didn't include JUDGING_PERMISSION for race crew in the default
 # config-roles file.  Delete and recreate the config-roles file.
 test -e /Library/WebServer/Documents/derbynet/local/config-roles.inc && \
