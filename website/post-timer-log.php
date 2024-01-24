68c3dc8ac6a8345388a2df82e78d4d026e75521a
Jeff Piazza <jeff@jeffpiazza.org>
2024-01-24 18:31:02 -0500

    If timer log file can't be opened for posting some text, pick a new file.

--- website/post-timer-log.php
+++ website/post-timer-log.php
@@ -2,12 +2,17 @@
 <?php
 
 require_once('inc/data.inc');
+require_once('inc/timer-logging.inc');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   header('Content-Type: text/xml; charset=utf-8');
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
   $post_data = fopen('php://input', 'r');
-  $log = fopen(read_raceinfo('timer-log'), 'a+');
+  $log_file = read_raceinfo('timer-log');
+  if (!$log_file) {
+    $log_file = set_timer_log_file();
+  }
+  $log = fopen($log_file, 'a+');
   if ($log !== false) {
     flock($log, LOCK_EX);
     $nbytes = stream_copy_to_stream($post_data, $log);
@@ -18,6 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo "<failure>Can't open ".read_raceinfo('timer-log')."\n";
     echo json_encode(error_get_last(), JSON_HEX_TAG | JSON_HEX_AMP | JSON_PRETTY_PRINT);
     echo "</failure>\n";
+    // Pick a new timer log file
+    set_timer_log_file();
   }
 } else {
     echo '<!DOCTYPE html><html><head><title>Not a Page</title></head><body><h1>This is not a page.</h1></body></html>';
