93c494a0d90345e9e6d213ab72cd8774c6d8a5e1
Jeff Piazza <jeff@jeffpiazza.org>
2024-03-21 19:16:31 -0400

    Fix security vulnerability exposed by kiosk.php page.

--- website/kiosk.php
+++ website/kiosk.php
@@ -16,7 +16,22 @@ function kiosk_parameters() {
 
 // 'page' query argument to support testing
 if (isset($_GET['page'])) {
-  require($_GET['page']);
+  // Confirm the requested page falls under the kiosks directory
+  // (Thank you, https://chocapikk.com !)
+  $self = dirname(realpath($_SERVER['SCRIPT_FILENAME']));
+  $page = realpath($self.'/'.$_GET['page']);
+  $kiosks_dir = $self.DIRECTORY_SEPARATOR.'kiosks';
+  for ($i = 1; true; ++$i) {
+    $dir = dirname($page, $i);
+    if ($dir == "/" || $dir == ".") {
+      $page = 'kiosks/identify.kiosk';
+      break;
+    }
+    if ($dir == $kiosks_dir) {
+      break;
+    }
+  }
+  require($page);
 } else {
   $kpage = kiosk_page(address_for_current_kiosk());
   $g_kiosk_parameters_string = $kpage['params'];
